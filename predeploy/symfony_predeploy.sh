b2c9c8cc8a87ab3ffc143e5fdddc12efc01b624c
Adrien Pestel <apestel@morea.fr>
2014-09-03 12:05:02 +0200

    change composer update to composer install

--- predeploy/symfony_predeploy.sh
+++ predeploy/symfony_predeploy.sh
@@ -5,7 +5,7 @@ export COMPOSER_HOME="."
 cp app/config/parameters_$ENV.yml app/config/parameters.yml
 php composer.phar self-update
 php composer.phar config -g github-oauth.github.com 891b5b98169ed0c08120fdec90cffaf8c292d7ba
-php composer.phar update
+#php composer.phar update
 php composer.phar install --prefer-source --no-interaction
 php app/console doctrine:schema:update --force
 php app/console assets:install web
