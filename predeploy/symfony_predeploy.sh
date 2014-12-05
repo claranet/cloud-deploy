7a89c1a6d26547b76867af521ab440380338f5f0
Adrien Pestel <apestel@morea.fr>
2014-12-05 17:55:36 +0000

    predeploy script update

--- predeploy/symfony_predeploy.sh
+++ predeploy/symfony_predeploy.sh
@@ -2,12 +2,12 @@
 # OAuth Token from morea-deploy account used for unattended deployement
 ENV=$1
 export COMPOSER_HOME="."
-cp app/config/parameters_$ENV.yml app/config/parameters.yml
+\cp -f app/config/parameters_$ENV.yml app/config/parameters.yml
 php composer.phar self-update
 php composer.phar config -g github-oauth.github.com 891b5b98169ed0c08120fdec90cffaf8c292d7ba
 #php composer.phar update
 php composer.phar install --prefer-source --no-interaction
-php app/console doctrine:schema:update --force
+php app/console doctrine:migrations:migrate --no-interaction
 php app/console assets:install web
 php app/console assets:install web --env=prod
 php app/console assetic:dump
