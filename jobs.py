b2a429e0291191e0a4ead2064296d3e8b8a8f641
Zied ABID <zabid@morea.fr>
2015-01-15 19:17:22 +0100

    update jobs schema

--- jobs.py
+++ jobs.py
@@ -1,21 +1,22 @@
 jobs_schema = {
-'command': {'type':'string', 'allowed':['deploy'],'required': True},
+'command': {'type':'string', 'allowed':['deploy','maintenance'],'required': True},
 'timestamp':{'type':'datetime','required': True},
 'parameters': {
 	'type':'dict',
 	'schema': {
-		'options' : {'type':'string'},
-		'app_id' : {'type': 'integer'},
-		'module' : {'type':'dict',
+        'options' : {'type':'list', 'items':[{'type':'string'}]},
+        'app_id' : {'type': 'integer', 'required': True},
+		'modules' : {'type':'dict',
 			'schema': {
 				'name' : {'type':'string'},
 				'rev'  : {'type':'string', 'default':'HEAD'}
 			}
-		} 
+		}
 	}
 
 },
-'status':{'type':'list', 'allowed':['pending','done','deleted']}
+'status':{'type':'list', 'readonly':True, 'allowed':['pending','done','deleted']}
+# TODO est ce que nous avons besoin du allowed alors ?
 
 }
 
@@ -23,3 +24,4 @@ jobs = {
 	'item_title': 'job',
 	'schema' :jobs_schema
 }
+
