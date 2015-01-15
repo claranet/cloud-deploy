c9fc9eaa1c9d383ad55d7d16aba7ce5b6e153a98
Zied ABID <zabid@morea.fr>
2015-01-15 18:23:59 +0100

    add regex to name and mails for apps

--- apps.py
+++ apps.py
@@ -6,26 +6,26 @@ import aws_data
 
 apps_schema = {
     # TODO required filds
-    # TODO validate name
-    'name' : {'type':'string'},#, 'regex':'^[a-zA-Z0-9_.]$'},
+    'name' : {'type':'string', 'regex':'^[a-zA-Z0-9_.+-]*$'},
     'aws_region' : {'type':'string', 'allowed':['us-east-1','eu-weast-1']},
     'modules': {'type':'list','schema':{
         'name': {'type':'string'},
         'git_repo' : {'type':'string'},
+        'scope' : {'type':'string', 'allowed':['system','code']},
         'code_deploy' : {'type':'dict', 'schema':code_deploy.code_deploy},
         'build_pack':{'type':'string'}}
     },
     'env': {'type':'string', 'allowed':env.env},
     'features':{'type':'list', 'allowed':salt_features.recipes},
     'role' : {'type':'string', 'allowed':instance_role.role},
-    'log_notifications' : {'type':'list','items':[{'type':'string'}]
-        #'regex':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}},
+    'log_notifications' : {'type':'list','items':[{'type':'string',
+        'regex':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}]
     },
     'ami': {'type':'string'},
     'instance_type': {'type':'string', 'allowed':aws_data.instance_type},
     'autoscale': { 'type': 'dict', 'schema': {
-        'min': {'type':'integer'},#, min:0},
-        'max': {'type':'integer'},#, min:1},
+        'mini': {'type':'integer', 'min':0},
+        'maxi': {'type':'integer', 'min':1},
         'current': {'type':'integer'}
         }
     }
