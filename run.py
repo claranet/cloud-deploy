441f853d8f8871a47f2f1fe12aaecbedc6a370a5
Zied ABID <zabid@morea.fr>
2015-01-15 18:42:02 +0100

    bind address

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from eve import Eve
 from apps import pre_GET_apps
+from flask.ext.bootstrap import Bootstrap
+from eve_docs import eve_docs
+
+
 app = Eve()
 
 app.on_pre_GET_apps += pre_GET_apps
 
+Bootstrap(app)
+app.register_blueprint(eve_docs, url_prefix='/docs')
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
