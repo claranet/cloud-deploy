72b2f81c055647a3be60438bb3a024236ddbd6e5
Zied ABID <zied@q2010.morea.fr>
2015-01-19 16:58:40 +0100

    adding app's dynamic ressources

--- apps.py
+++ apps.py
@@ -3,21 +3,22 @@ import code_deploy
 import instance_role
 import salt_features
 import aws_data
+import ressources
 
 apps_schema = {
     # TODO required filds
-    'name' : {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$'},
-    'aws_region' : {'type': 'string', 'allowed':['us-east-1','eu-weast-1']},
+    'name': {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$'},
+    'aws_region': {'type': 'string', 'allowed':['us-east-1','eu-weast-1']},
     'instance_type': {'type': 'string', 'allowed':aws_data.instance_type},
     'env': {'type': 'string', 'allowed':env.env},
-    'features':{'type': 'list', 'allowed':salt_features.recipes},
-    'role' : {'type': 'string', 'allowed':instance_role.role},
-    'ami': {'type': 'string'},
-    'vpc': {'type': 'string'},
+    'features':{'type':'list', 'allowed':salt_features.recipes},
+    'role': {'type':'string', 'allowed':instance_role.role},
+    'ami': {'type':'string'},
+    'vpc': {'type':'string'},
     'modules': {'type':'list','schema':{
         'name': {'type':'string'},
-        'git_repo' : {'type':'string'},
-        'scope' : {'type':'string', 'allowed':['system','code']},
+        'git_repo': {'type':'string'},
+        'scope': {'type':'string', 'allowed':['system','code']},
         'code_deploy' : {'type':'dict', 'schema':code_deploy.code_deploy},
         'build_pack':{'type':'media'}}
     },
@@ -30,20 +31,13 @@ apps_schema = {
         'current': {'type':'integer'}
         }
     },
-    # TODO solve dynamic schema (rds-mysql, ec-redis, ec-memcached...)
     # TODO solve storing password in cleartext
-    'ressources': {'type':'list', 'schema': {
-        '_type': { 'type': 'string', 'allowed':['rds-mysql'] },
-        'hostname': {'type': 'string'},
-        'database': {'type': 'string'},
-        'login': {'type': 'string'},
-        'password': {'type': 'string'}}
-    }
+    'ressources': {'type':'list', 'allowed':ressources.available}
 }
 
 apps = {
-'item_title' : 'app',
-'schema' : apps_schema
+'item_title': 'app',
+'schema': apps_schema
 }
 
 
---
+++ ressources.py
@@ -0,0 +1,18 @@
+available = [
+
+ {'rds-mysql': { 'type':'dict', 'schema': {
+        'name': { 'type': 'string', },
+        'hostname': {'type': 'string'},
+	'port' : {'type': 'integer', 'min':1, 'max':65535},	
+        'database': {'type': 'string'},
+        'login': {'type': 'string'},
+        'password': {'type': 'string'} 
+	}
+  }},
+ {'redis': { 'type':'dict', 'schema': {
+        'name': { 'type': 'string', },
+        'uri': {'type': 'string'}
+	}
+  }}
+
+]
