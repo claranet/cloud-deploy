f347f2518e6af5b20054bf56ca5116ca4ca3698e
Adrien Pestel <apestel@morea.fr>
2015-01-19 16:20:39 +0100

    add ressources

--- apps.py
+++ apps.py
@@ -6,13 +6,14 @@ import aws_data
 
 apps_schema = {
     # TODO required filds
-    'name' : {'type':'string', 'regex':'^[a-zA-Z0-9_.+-]*$'},
-    'aws_region' : {'type':'string', 'allowed':['us-east-1','eu-weast-1']},
-    'instance_type': {'type':'string', 'allowed':aws_data.instance_type},
-    'env': {'type':'string', 'allowed':env.env},
-    'features':{'type':'list', 'allowed':salt_features.recipes},
-    'role' : {'type':'string', 'allowed':instance_role.role},
-    'ami': {'type':'string'},
+    'name' : {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$'},
+    'aws_region' : {'type': 'string', 'allowed':['us-east-1','eu-weast-1']},
+    'instance_type': {'type': 'string', 'allowed':aws_data.instance_type},
+    'env': {'type': 'string', 'allowed':env.env},
+    'features':{'type': 'list', 'allowed':salt_features.recipes},
+    'role' : {'type': 'string', 'allowed':instance_role.role},
+    'ami': {'type': 'string'},
+    'vpc': {'type': 'string'},
     'modules': {'type':'list','schema':{
         'name': {'type':'string'},
         'git_repo' : {'type':'string'},
@@ -24,10 +25,19 @@ apps_schema = {
         'regex':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}]
     },
     'autoscale': { 'type': 'dict', 'schema': {
-        'mini': {'type':'integer', 'min':0},
-        'maxi': {'type':'integer', 'min':1},
+        '_min': {'type':'integer', 'min':0},
+        '_max': {'type':'integer', 'min':1},
         'current': {'type':'integer'}
         }
+    },
+    # TODO solve dynamic schema (rds-mysql, ec-redis, ec-memcached...)
+    # TODO solve storing password in cleartext
+    'ressources': {'type':'list', 'schema': {
+        '_type': { 'type': 'string', 'allowed':['rds-mysql'] },
+        'hostname': {'type': 'string'},
+        'database': {'type': 'string'},
+        'login': {'type': 'string'},
+        'password': {'type': 'string'}}
     }
 }
 
