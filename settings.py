abf9af169c537ad0a94e65e6ac38479a4972e92c
Zied ABID <zabid@morea.fr>
2015-02-12 12:08:30 +0100

    prevent delete all apps

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ MONGO_DBNAME = 'apitest'
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
 # read-only access to the endpoint).
-RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+RESOURCE_METHODS = ['GET', 'POST']
 
 # Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
 # individual items  (defaults to read-only item access).
