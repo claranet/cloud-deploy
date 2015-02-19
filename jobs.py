e351d9c48bf02018c40cce2c4f1580d197beb719
Zied ABID <zabid@morea.fr>
2015-02-19 17:48:02 +0100

    pretty code

--- jobs.py
+++ jobs.py
@@ -1,23 +1,21 @@
 jobs_schema = {
-'command': {'type':'string', 'allowed':['deploy','maintenance'],'required': True},
-'app_id' : {'type': 'string', 'regex':'^[a-f0-9]{24}$','required': True},
-'job_id' : {'type': 'string', 'regex': '^[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}$', 'readonly': True},
-'status':{'type':'list', 'readonly':True},
+'command': {'type':'string',
+            'allowed':['deploy','maintenance'],
+            'required': True},
+'app_id' : {'type': 'string',
+            'regex':'^[a-f0-9]{24}$',
+            'required': True},
+'job_id' : {'type': 'string',
+            'regex': '^[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}$',
+            'readonly': True},
+'status':{'type':'string', 'readonly':True},
 'user': {'type':'string','required':True},
-'parameters': {
-	'type':'dict',
-	'schema': {
-        'options' : {'type':'list', 'schema':{'type':'string'}},
-        'modules' : {'type':'list',
-			'schema': {
-				'name' : {'type':'string'},
-				'rev'  : {'type':'string', 'default':'HEAD'}
-			}
-		}
-	}
-
-}
-
+'options' : {'type':'list', 'schema':{'type':'string'}},
+'modules' : {'type':'list', 'schema': {
+    	     'name':{'type':'string'},
+             'rev' :{'type':'string', 'default':'HEAD'}
+	            }
+            }
 }
 
 jobs = {
