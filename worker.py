c54a295dd84f7ae55ef5bf4b1e6a4fd7431e75f0
Adrien Pestel <apestel@morea.fr>
2015-02-24 11:34:12 +0100

    worker functional

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 from subprocess import call
 import datetime
 import logging
+import yaml
 from settings import MONGO_DBNAME
 from boto import sns
 from boto.ec2 import autoscale
@@ -12,9 +13,6 @@ from pprint import pprint
 from notification import Notification
 import os
 
-from commands import *
-
-
 LOG_PATH='/var/log/ghost'
 ROOT_PATH=os.path.dirname(os.path.realpath(__file__))
 MAIL_LOG_FROM='no-reply@morea.fr'
@@ -22,16 +20,21 @@ MAIL_LOG_FROM='no-reply@morea.fr'
 class Worker:
     _config = None
     _worker_job = None
-    _job = None
+    job = None
     _db = None
-    _log_file = None
-    _app = None
+    log_file = None
+    app = None
 
     def __init__(self, dry_run=False):
         self._dry_run = dry_run
+        rootdir = os.path.dirname(os.path.realpath(__file__))
+        conf_file_path = rootdir + "/config.yml"
+        conf_file = open(conf_file_path, 'r')
+        self._config = yaml.load(conf_file)
+
 
     def _log(self, message):
-        self._log_file.write("{message}\n".format(message=message))
+        self.log_file.write("{message}\n".format(message=message))
 
 
     def _connect_db(self):
@@ -42,6 +45,7 @@ class Worker:
         MongoClient().disconnect()
 
 
+    #FIXME: not used anymore
     def _update_progress(self, message, **kwargs):
         self._worker_job.meta['progress_message'] = message
         if kwargs and kwargs['percent']:
@@ -49,26 +53,30 @@ class Worker:
         self._worker_job.save()
 
 
-    def _update_task(self, task_status, message=None):
-        self._task['status'] = task_status
-        if message:
-            self._task['message'] = message
-        self._db.tasks.update({ '_id': self._task['_id']}, {'$set': {'status': task_status, 'message': message, 'updated_at': datetime.datetime.now()}})
+    def update_status(self, status, message=None):
+        #self._task['status'] = task_status
+        #if message:
+        #    self._task['message'] = message
+        self.job['status'] = status
+        self._db.jobs.update({ '_id': self.job['_id']}, {'$set': {'status': status, 'message': message, 'updated_at': datetime.datetime.now()}})
+
+    def module_initialized(self, module_name):
+        self._db.apps.update({ '_id': self.app['_id'], 'modules.name': module_name}, {'$set': { 'modules.$.initialized': True }})
 
 
     def _init_log_file(self):
-        self._log_file = open("{log_path}/{job_id}".format(log_path=LOG_PATH, job_id=self._worker_job.id), 'a', 1)
+        self.log_file = open("{log_path}/{job_id}.txt".format(log_path=LOG_PATH, job_id=self._worker_job.id), 'a', 1)
 
 
     def _close_log_file(self):
-        self._log_file.close()
+        self.log_file.close()
 
 
     def _format_notif(self):
-        title = "App: {app_name} - {action} : {status}".format(app_name=self._app['app'], action=self._task['action'], status=self._task['status'])
-        message = "Application: {app_name}\nEnvironment: {env}\nAction: {action}\nStatus: {status}".format(env=self._app['env'], app_name=self._app['app'], action=self._task['action'], status=self._task['status'])
-        if 'error_message' in self._task.keys():
-            message = "{message}\nError: {error_message}".format(message=message, error_message=self._task['error_message'])
+        title = "App: {app_name} - {action} : {status}".format(app_name=self.app['name'], action=self.job['command'], status=self.job['status'])
+        message = "Application: {app_name}\nEnvironment: {env}\nAction: {action}\nStatus: {status}".format(env=self.app['env'], app_name=self.app['name'], action=self.job['command'], status=self.job['status'])
+        if 'error_message' in self.job.keys():
+            message = "{message}\nError: {error_message}".format(message=message, error_message=self.job['error_message'])
         return title, message
 
 
@@ -76,8 +84,8 @@ class Worker:
         ses_settings = self._config['ses_settings']
         notif = Notification(aws_access_key=ses_settings['aws_access_key'], aws_secret_key=ses_settings['aws_secret_key'], region=ses_settings['region'])
         subject, body = self._format_notif()
-        log = "{log_path}/{job_id}".format(log_path=LOG_PATH, job_id=self._worker_job.id)
-        for mail in self._app['log_notifications']:
+        log = "{log_path}/{job_id}.txt".format(log_path=LOG_PATH, job_id=self._worker_job.id)
+        for mail in self.app['log_notifications']:
             notif.send_mail(From=MAIL_LOG_FROM, To=mail, subject=subject, body=body, attachments=[log])
 
 
@@ -85,11 +93,13 @@ class Worker:
         with Connection():
             self._worker_job = get_current_job()
         self._connect_db()
-        self._job = self._db.jobs.find_one({'_id': ObjectId(job_id)})
-        self._app = self._db.apps.find_one({'_id': ObjectId(self._job['app_id'])})
+        self.job = self._db.jobs.find_one({'_id': ObjectId(job_id)})
+        self.app = self._db.apps.find_one({'_id': ObjectId(self.job['app_id'])})
         self._init_log_file()
-        #FIXME INTROSPECTION ie: commands/deploy Deploy
-        result = Deploy(self._app, self._job, self._log_file)
+        klass_name = self.job['command'].title()
+        mod = __import__('commands.' + self.job['command'], fromlist=[klass_name])
+        command = getattr(mod, klass_name)(self)
+        result = command.execute()
         self._close_log_file()
         self._mail_log_action()
         self._disconnect_db()
