a04b66605c535a348d80604060b094e693b07d7b
root <root@ip-10-10-1-202.eu-west-1.compute.internal>
2015-02-24 10:14:10 +0000

    fix for region in apps.py

--- apps.py
+++ apps.py
@@ -6,26 +6,19 @@ import aws_data
 import ressources
 
 apps_schema = {
-    # TODO required filds
-    'name': {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$'},
+    'name': {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$', 'required':True },
     'region': {'type': 'string', 'allowed':['us-east-1','eu-west-1']},
     'instance_type': {'type': 'string', 'allowed':aws_data.instance_type},
-    'env': {'type': 'string', 'allowed':env.env},
+    'env': {'type': 'string', 'allowed':env.env, 'required':True},
     'features':{'type':'list', 'schema':salt_features.recipes},
-    'role': {'type':'string', 'allowed':instance_role.role},
+    'role': {'type':'string', 'allowed':instance_role.role,'required':True},
     'ami': {'type':'string', 'regex': '^ami-[a-z0-9]*$'},
-    'source_ami' : {'type': 'string', 'regex': '^ami-[a-z0-9]*$'},
-    'ami_name': {'type': 'string'},
-    'ssh_username': {'type': 'string','regex': '^[a-z0-9]*$'},
-    'subnet_id': {'type': 'regex': '^subnet-[a-z0-9]*$'},
-    'vpc_id': {'type':'string', 'regex': '^vpc-[a-z0-9]*$'},
-    'associate_public_ip_address': {'type': 'integer', 'min':0, 'max':1},
+    'vpc_id': {'type':'string', 'regex': '^vpc-[a-z0-9]*$', 'required':True},
     'modules': {'type':'list','schema':{ 'type':'dict', 'schema': {
         'initialized': {'type':'boolean', 'readonly':True},
-        'name': {'type':'string'},
+        'name': {'type':'string', 'required':True},
         'git_repo': {'type':'string', 'required':True},
         'scope': {'type':'string', 'required':True,'allowed':['system','code']},
-        #'code_deploy' : {'type':'dict', 'schema':code_deploy.code_deploy},
         'build_pack':{'type':'media'},
         'pre_deploy':{'type':'media'},
         'post_deploy':{'type':'media'},
@@ -35,9 +28,18 @@ apps_schema = {
         'regex':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}
     },
     'autoscale': { 'type': 'dict', 'schema': {
-        '_min': {'type':'integer', 'min':0},
-        '_max': {'type':'integer', 'min':1},
+        'min': {'type':'integer', 'min':0},
+        'max': {'type':'integer', 'min':1},
         'current': {'type':'integer'}
+        }
+    },
+    'build_infos':{ 'type': 'dict', 'schema': {
+        'ssh_username': {'type': 'string', 'required':True},
+        'source_ami' : {'type': 'string', 'regex': '^ami-[a-z0-9]*$', 'required':True},
+        'ami_name' : {'type': 'string', 'required':True},
+        'subnet_id': {'type': 'string','regex': '^subnet-[a-z0-9]*$', 'required':True},
+        'associate_EIP': {'type':'string',
+                          'regex': '^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$'},
         }
     },
     # TODO solve storing password in cleartext
@@ -48,7 +50,3 @@ apps = {
 'item_title': 'app',
 'schema': apps_schema
 }
-
-
-#def pre_GET_apps(request, lookup):
-#    print 'A GET request on apps endpoint has just been received!'
