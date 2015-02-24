ed5f276cde6f63a6b9a99df9b40f66a5b1ed049f
Adrien Pestel <apestel@morea.fr>
2015-02-24 15:55:42 +0100

    Can deploy on 0 host => for init

--- fabfile.py
+++ fabfile.py
@@ -4,13 +4,16 @@ from commands.tools import GCallException
 
 def find_ec2_instances(ghost_app, ghost_env, ghost_role, region):
     conn = ec2.connect_to_region(region)
-    reservations = conn.get_all_instances(filters={"tag:Env": ghost_env, "tag:Role": ghost_role, "tag:App": ghost_app, "instance-state-name":"running"})
+    reservations = conn.get_all_instances(filters={"tag:Env": ghost_env, \
+            "tag:Role": ghost_role, "tag:App": ghost_app, \
+            "instance-state-name":"running"})
     hosts = []
     for reservation in reservations:
         for instance in reservation.instances:
             hosts.append(instance.private_ip_address)
-    if (len(hosts) == 0):
-        raise CallException("No instance found with tags App:%s, Role:%s, Env:%s" % (ghost_app, ghost_role, ghost_env))
+    #if (len(hosts) == 0):
+    #    raise GCallException("No instance found with tags App:%s, Role:%s, Env:%s" \
+            #            % (ghost_app, ghost_role, ghost_env))
     return hosts
 
 @task
