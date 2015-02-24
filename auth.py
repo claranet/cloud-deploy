212a199e62e80c28a7e7299d1af7513dab451419
Adrien Pestel <apestel@morea.fr>
2015-02-24 15:02:26 +0100

    add python2 workaround for auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+import sys
 import bcrypt
 from eve.auth import BasicAuth
 
@@ -9,7 +10,10 @@ class BCryptAuth(BasicAuth):
 
     def check_auth(self, username, password, allowed_roles, resource, method):
         # use Eve's own db driver; no additional connections/resources are used
-        account = next(item for item in self._accounts if item["user"] == username)
+        if sys.version > '3':
+            account = next(item for item in self._accounts if item["user"] == username)
+        else:
+            account = (item for item in self._accounts if item["user"] == username).next()
         #return account and bcrypt.hashpw(password, account['pass']) == account['pass']
         return account and password == account['pass']
 
