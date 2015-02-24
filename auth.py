5133d239b23c41827aa868ad2c89e6147f3d2303
Adrien Pestel <apestel@morea.fr>
2015-02-24 11:33:08 +0100

    support for python3

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ class BCryptAuth(BasicAuth):
 
     def check_auth(self, username, password, allowed_roles, resource, method):
         # use Eve's own db driver; no additional connections/resources are used
-        account = (item for item in self._accounts if item["user"] == username).next()
+        account = next(item for item in self._accounts if item["user"] == username)
         #return account and bcrypt.hashpw(password, account['pass']) == account['pass']
         return account and password == account['pass']
 
