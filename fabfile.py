844709c1f8c952a69fe3cd9efb8078e430939779
Adrien Pestel <apestel@morea.fr>
2015-02-24 11:35:00 +0100

    add check on running instance, and fix reservation bug

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,19 @@
 from fabric.api import run, sudo, task, env, put
 from boto import ec2
-from task import CallException
+from commands.tools import GCallException
 
 def find_ec2_instances(ghost_app, ghost_env, ghost_role, region):
     conn = ec2.connect_to_region(region)
     #reservations = conn.get_all_instances()
-    reservations = conn.get_all_instances(filters={"tag:Env": ghost_env, "tag:Role": ghost_role, "tag:App": ghost_app})
+    reservations = conn.get_all_instances(filters={"tag:Env": ghost_env, "tag:Role": ghost_role, "tag:App": ghost_app, "instance-state-name": "running"})
     res = []
     for reservation in reservations:
-        instance = reservation.instances[0]
-        if instance.private_ip_address:
-            res.append(instance.private_ip_address)
+        instances = reservation.instances
+        for instance in instances:
+            if instance.private_ip_address:
+                res.append(instance.private_ip_address)
     if (len(res) == 0):
-        raise CallException("No instance found with tags App:%s, Role:%s, Env:%s" % (ghost_app, ghost_role, ghost_env))
+        raise GCallException("No instance found with tags App:%s, Role:%s, Env:%s" % (ghost_app, ghost_role, ghost_env))
     return res
 
 @task
