2790727943b802587dd969694ee9f86e1406022d
Zied ABID <zabid@gmail.com>
2015-03-02 20:05:43 +0000

    succefull app registration with one module, without build info neither autoscale

--- front.py
+++ front.py
@@ -5,11 +5,23 @@ import env
 import base64
 import tempfile
 import os
+import requests
+import json
 
 def get_vpc():
    return ['vpc-12345', 'vpc-7896']
 
 
+def convert_to_base64(script):
+    p,buildpack_path = tempfile.mkstemp()
+    buildfile = open(buildpack_path,'w')
+    request.files[script].save(buildfile)
+    buildfile.close
+    with open(buildpack_path, "rb") as buildfile:
+        result = base64.b64encode(buildfile.read())
+    buildfile.close
+    os.remove(buildpack_path)
+    return result
 
 app = Flask(__name__)
 
@@ -25,17 +37,10 @@ def res():
     module = {}
     build_infos = {}
     autoscale = {}
-
     if 'module-build_pack' in dict(request.files).keys():
-        p,buildpack_path = tempfile.mkstemp()
-        buildfile = open(buildpack_path,'w')
-        request.files['module-build_pack'].save(buildfile)
-        buildfile.close
-        with open(buildpack_path, "rb") as buildfile:
-            module['build_pack'] = base64.b64encode(buildfile.read())
-        buildfile.close
-        os.remove(buildpack_path)
-
+        module['build_pack'] = convert_to_base64('module-build_pack')
+    if 'module-post_deploy' in dict(request.files).keys():
+        module['post_deploy'] = convert_to_base64('module-post_deploy')
     for key in request.form.keys():
         if key.find('module') >= 0:
             module[key[7:]] = request.form[key]
@@ -45,11 +50,15 @@ def res():
             autoscale[key[11:]] = request.form[key]
         else:
             app[key] = request.form[key]
-    app['autoscale'] = autoscale
-    app['build_infos'] = build_infos
+    #app['autoscale'] = autoscale
+    #app['build_infos'] = build_infos
     app['modules'] = [module]
     print app
-    resp = make_response("<a href='/app_form'>return</a>")
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    url = 'http://localhost:5000/apps'
+    eve_response = requests.post(url, data=json.dumps(app), headers=headers, auth=('api','api'))
+    print(eve_response.content)
+    resp = make_response(eve_response.content+"</br><a href='/app_form'>return</a>")
     return resp
 
 
