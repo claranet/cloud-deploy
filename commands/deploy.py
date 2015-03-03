8778661d49ce4b01999bcb7bdbdc6543cd5dc2cf
Zied ABID <zabid@gmail.com>
2015-03-03 10:41:39 +0000

    add region to aws command

--- commands/deploy.py
+++ commands/deploy.py
@@ -101,8 +101,8 @@ class Deploy():
         os.chdir(self._get_path_from_module(module))
         pkg_name = "{0}_{1}_{2}".format(ts, module['name'], commit)
         gcall("tar cvzf ../%s . > /dev/null" % pkg_name, "Creating package: %s" % pkg_name, self._log_file)
-        gcall("aws s3 cp ../{0} s3://{bucket_s3}{path}/".format(pkg_name, \
-                bucket_s3=self._config['bucket_s3'], path=self._get_path_from_module(module)), "Uploading package: %s" % pkg_name, self._log_file)
+        gcall("aws --region {region} s3 cp ../{0} s3://{bucket_s3}{path}/".format(pkg_name, \
+                bucket_s3=self._config['bucket_s3'], region=self._app['region'], path=self._get_path_from_module(module)), "Uploading package: %s" % pkg_name, self._log_file)
         gcall("rm -f ../{0}".format(pkg_name), "Deleting local package: %s" % pkg_name, self._log_file)
         return pkg_name
 
