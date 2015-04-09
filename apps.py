0ae7e7a48f1b2b9383b7a66a9da6a168a23ba27d
Patrick Decat <pdecat@morea.fr>
2015-04-09 15:48:10 +0200

    Format apps.py file

--- apps.py
+++ apps.py
@@ -6,7 +6,7 @@ import ressources
 
 apps_schema = {
     'name': {'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]*$', 'required':True },
-    'region': {'type': 'string', 'allowed':['us-east-1','eu-west-1']},
+    'region': {'type': 'string', 'allowed':['us-east-1', 'eu-west-1']},
     'instance_type': {'type': 'string', 'allowed':aws_data.instance_type},
     'env': {'type': 'string', 'allowed':env.env, 'required':True},
     'features': {
@@ -19,20 +19,20 @@ apps_schema = {
             }
         }
     },
-    'role': {'type':'string', 'allowed':instance_role.role,'required':True},
+    'role': {'type':'string', 'allowed':instance_role.role, 'required':True},
     'ami': {'type':'string', 'regex': '^ami-[a-z0-9]*$', 'readonly':True },
     'vpc_id': {'type':'string', 'regex': '^vpc-[a-z0-9]*$', 'required':True},
-    'modules': {'type':'list','schema':{ 'type':'dict', 'schema': {
+    'modules': {'type':'list', 'schema':{ 'type':'dict', 'schema': {
         'initialized': {'type':'boolean', 'readonly':True},
         'name': {'type':'string', 'required':True},
         'git_repo': {'type':'string', 'required':True},
-        'scope': {'type':'string', 'required':True,'allowed':['system','code']},
+        'scope': {'type':'string', 'required':True, 'allowed':['system', 'code']},
         'build_pack':{'type':'string'},
         'pre_deploy':{'type':'string'},
         'post_deploy':{'type':'string'},
         'path':{'type':'string', 'required':True}}}
     },
-    'log_notifications' : {'type':'list','schema':{'type':'string',
+    'log_notifications' : {'type':'list', 'schema':{'type':'string',
         'regex':'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}
     },
     'autoscale': { 'type': 'dict', 'schema': {
@@ -46,14 +46,14 @@ apps_schema = {
         'ssh_username': {'type': 'string', 'required':True},
         'source_ami' : {'type': 'string', 'regex': '^ami-[a-z0-9]*$', 'required':True},
         'ami_name' : {'type': 'string', 'required':True},
-        'subnet_id': {'type': 'string','regex': '^subnet-[a-z0-9]*$', 'required':True}
+        'subnet_id': {'type': 'string', 'regex': '^subnet-[a-z0-9]*$', 'required':True}
         }
     },
     # TODO solve storing password in cleartext
     'ressources': {'type':'list', 'schema':ressources.available},
     'environment_infos': {'type': 'dict', 'schema': {
-        'security_groups' : {'type':'list','schema':{'type':'string', 'regex': '^sg-[a-z0-9]*$'}},
-        'subnet_ids' : {'type':'list','schema':{'type':'string', 'regex': '^subnet-[a-z0-9]*$'}},
+        'security_groups' : {'type':'list', 'schema':{'type':'string', 'regex': '^sg-[a-z0-9]*$'}},
+        'subnet_ids' : {'type':'list', 'schema':{'type':'string', 'regex': '^subnet-[a-z0-9]*$'}},
         'instance_profile' : {'type': 'string'},
         'key_name' : {'type': 'string'}
         }
