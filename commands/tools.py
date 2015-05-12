626450f8c20a90c25fd37109eafcacff1d39bbad
Kevin <kpecquet@morea.fr>
2015-05-12 12:46:28 +0000

    add boto.autoscale exists check in buildimage. check_autoscale_exists function is in tools.py

--- commands/tools.py
+++ commands/tools.py
@@ -38,26 +38,10 @@ def find_ec2_instances(ghost_app, ghost_env, ghost_role, region):
 def log(message, fd):
     fd.write("{message}\n".format(message=message))
 
-#def create_launch_config(app, userdata):
-#    d = time.strftime('%d%m%Y-%H%M',time.localtime())
-#    conn = ec2.connect_to_region(app['region'])
-#    launch_config = LaunchConfiguration(connection=conn, \
-#        name="launchconfig.{0}.{1}.{2}.{3}".format(app['env'], app['region'], app['name'], d), \
-#        image_id=app['ami'], key_name=app['environment_infos']['key_name'], \
-#        security_groups=app['environment_infos']['security_groups'], \
-#        user_data=userdata, instance_type=app['instance_type'], kernel_id=None, \
-#        ramdisk_id=None, block_device_mappings=None, \
-#        instance_monitoring=False, spot_price=None, \
-#        instance_profile_name=app['environment_infos']['instance_profile'], ebs_optimized=False, \
-#        associate_public_ip_address=True, volume_type=None, \
-#        delete_on_termination=True, iops=None, \
-#        classic_link_vpc_id=None, classic_link_vpc_security_groups=None)
-#    conn.create_launch_configuration(launch_config)
-
 def create_launch_config(app, userdata, ami_id):
     d = time.strftime('%d%m%Y-%H%M',time.localtime())
     launch_config_name = "launchconfig.{0}.{1}.{2}.{3}".format(app['env'], app['region'], app['name'], d)
-    conn = boto.ec2.autoscale.connect_to_region(app['region'])
+    conn_as = boto.ec2.autoscale.connect_to_region(app['region'])
     launch_config = LaunchConfiguration(name=launch_config_name, \
         image_id=ami_id, key_name=app['environment_infos']['key_name'], \
         security_groups=app['environment_infos']['security_groups'], \
@@ -68,7 +52,7 @@ def create_launch_config(app, userdata, ami_id):
         associate_public_ip_address=True, volume_type=None, \
         delete_on_termination=True, iops=None, \
         classic_link_vpc_id=None, classic_link_vpc_security_groups=None)
-    conn.create_launch_configuration(launch_config)
+    conn_as.create_launch_configuration(launch_config)
     return launch_config
 
 
@@ -80,3 +64,11 @@ def generate_userdata(bucket_s3, root_ghost_path):
         template = jinja_env.get_template('bootstrap.sh')
         userdata = template.render(bucket_s3=bucket_s3)
         return userdata
+
+def check_autoscale_exists(as_name, region):
+    conn_as = boto.ec2.autoscale.connect_to_region(region)
+    autoscale = conn_as.get_all_groups(names=[as_name])
+    if autoscale:
+        return True
+    else:
+        return False
