d7d7a5a7225ea70872599f4d31f07c3acb1dde1b
Kevin <kpecquet@morea.fr>
2015-06-17 15:55:58 +0000

    fix indentation and remove require on ami_name field

--- apps.py
+++ apps.py
@@ -33,70 +33,53 @@ apps_schema = {
     'modules': {
         'type': 'list', 'schema': {
             'type': 'dict', 'schema': {
-                'initialized': {'type': 'boolean', 'readonly': True
-                        }, 'name': {'type': 'string', 'required': True
-                 }, 'git_repo': {'type': 'string', 'required': True
-                     }, 'scope': {
-                         'type': 'string', 'required': True, 'allowed': [
-                             'system', 'code'
-                         ]
-                     }, 'build_pack': {'type': 'string'
-                      }, 'pre_deploy': {'type': 'string'
-                      }, 'post_deploy': {'type': 'string'
-                       }, 'path': {'type': 'string', 'required': True}
+                'initialized': {'type': 'boolean', 'readonly': True},
+                'name': {'type': 'string', 'required': True},
+                'git_repo': {'type': 'string', 'required': True},
+                'scope': {
+                    'type': 'string', 'required': True, 'allowed': ['system', 'code']
+                },
+                'build_pack': {'type': 'string'},
+                'pre_deploy': {'type': 'string'},
+                'post_deploy': {'type': 'string'},
+                'path': {'type': 'string', 'required': True}
             }
         }
     },
     'log_notifications': {
         'type': 'list', 'schema': {
-            'type': 'string', 'regex':
-            '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            'type': 'string',
+            'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         }
     },
     'autoscale': {
         'type': 'dict', 'schema': {
-            'min': {'type': 'integer', 'min': 0
-                }, 'max': {'type': 'integer', 'min': 1
-                }, 'current': {'type': 'integer'}, 'name': {'type': 'string'}
+            'min': {'type': 'integer', 'min': 0},
+            'max': {'type': 'integer', 'min': 1},
+            'current': {'type': 'integer'},
+            'name': {'type': 'string'}
         }
     },
     'build_infos': {
         'type': 'dict', 'schema': {
-            'ssh_username': {'type': 'string', 'required': True}, 'source_ami':
-            {'type': 'string', 'regex': '^ami-[a-z0-9]*$', 'required': True
-                        }, 'ami_name': {'type': 'string', 'required': True
-                      }, 'subnet_id': {
-                          'type': 'string', 'regex': '^subnet-[a-z0-9]*$',
-                          'required': True
-                      }
+            'ssh_username': {'type': 'string', 'required': True},
+            'source_ami': {'type': 'string', 'regex': '^ami-[a-z0-9]*$', 'required': True},
+            'ami_name': {'type': 'string', 'required': False},
+            'subnet_id': {'type': 'string', 'regex': '^subnet-[a-z0-9]*$', 'required': True}
         }
     },
     'ressources': {'type': 'list', 'schema': ressources.available},
-    'environment_infos': {
-        'type': 'dict', 'schema': {
-            'security_groups': {
-                'type': 'list', 'schema': {
-                    'type': 'string', 'regex': '^sg-[a-z0-9]*$'
-                }
-            }, 'subnet_ids': {
-                'type': 'list', 'schema': {
-                    'type': 'string', 'regex': '^subnet-[a-z0-9]*$'
-                }
-            }, 'instance_profile': {'type': 'string'
-                              }, 'key_name': {'type': 'string'
-                      }, 'root_block_device': {
-                          'type': 'dict', 'schema': {
-                              'size': {'type': 'integer'
-                     }, 'name': {
-                         'type': 'string', 'regex': '^/[a-z0-9]+/[a-z0-9]+$'
-                     }
-                          }
-                      }
-        }
-    },
-    'user': {
-        'type': 'string'
-    },
+    'environment_infos': {'type': 'dict', 'schema': {
+        'security_groups': {'type': 'list', 'schema': {'type': 'string', 'regex': '^sg-[a-z0-9]*$'}},
+        'subnet_ids': {'type': 'list', 'schema': {'type': 'string', 'regex': '^subnet-[a-z0-9]*$'}},
+        'instance_profile': {'type': 'string'},
+        'key_name': {'type': 'string'},
+        'root_block_device': {'type': 'dict', 'schema': {
+            'size': {'type': 'integer'},
+            'name': {'type': 'string', 'regex': '^/[a-z0-9]+/[a-z0-9]+$'}
+        }}
+    }},
+    'user': {'type': 'string'},
 }
 
 apps = {'item_title': 'app', 'schema': apps_schema}
