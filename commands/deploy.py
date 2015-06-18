c67cd3cbc3c6d9b586dd803008049d8243f77085
Patrick Decat <pdecat@morea.fr>
2015-06-18 17:49:16 +0200

    Fix #34: persist deployment id in jobs (no inserted_id with PyMongo < 3.0)

--- commands/deploy.py
+++ commands/deploy.py
@@ -270,4 +270,4 @@ class Deploy():
             self._start_autoscale()
         self._purge_old_modules(module)
         deployment = {'app_id': self._app['_id'], 'job_id': self._job['_id'], 'module': module['name'], 'commit': commit, 'timestamp': ts, 'package': pkg_name, 'module_path': module['path']}
-        return self._worker._db.deploy_histories.insert(deployment).inserted_id
+        return self._worker._db.deploy_histories.insert(deployment)
