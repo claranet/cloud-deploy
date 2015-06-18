45a524f438f6d94d5263f535ce0fda5f632b51cb
Patrick Decat <pdecat@morea.fr>
2015-06-18 22:47:59 +0200

    Fix #34: job module deploy_id field must not be read-only to allow rollbacks

--- jobs.py
+++ jobs.py
@@ -33,7 +33,7 @@ jobs_schema = {
                 }, 'rev': {
                     'type': 'string', 'default': 'HEAD'
                 }, 'deploy_id': {
-                    'type': 'string', 'readonly': True
+                    'type': 'string'
                 }
             }
         }
