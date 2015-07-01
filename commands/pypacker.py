de6dfa7fde3cc841c877c717256e884e3cfb3bfa
Kevin <kpecquet@morea.fr>
2015-07-01 13:54:12 +0000

    fix logs of packer

--- commands/pypacker.py
+++ commands/pypacker.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from sh import packer, git
+from commands.tools import log
 import sh
 import logging
 import yaml
@@ -88,13 +89,9 @@ class Packer:
         if not os.path.isdir('/tmp/root'):
             os.makedirs('/tmp/root')
         try:
-            #new_env = os.environ.copy()
-            #new_env['PACKER_LOG'] = '1'
-            #new_env['PACKER_LOG_PATH'] = SALT_LOCAL_TREE + self.unique + '/packer.log'
-            #result = packer.build(self.packer_file_path, _env=new_env)
-            result = packer.build(self.packer_file_path, _err=self._log_file)
+            result = packer.build(self.packer_file_path)
             ami = re.findall('ami-[a-z0-9]*$', result.rstrip())[0]
-            logging.debug(result)
+            log(result, self._log_file)
         except sh.ErrorReturnCode as e:
             ami = "ERROR"
             logging.error(e)
