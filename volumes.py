684131a272597695d3f474fd697afbba13396c38
Adrien Pestel <apestel@morea.fr>
2015-07-03 12:29:46 +0200

    add volume schema

---
+++ volumes.py
@@ -0,0 +1,6 @@
+block = {'type': 'dict', 'schema': {
+    'device_name': {'type': 'string', 'regex': '^/dev/xvd[b-m]$', 'required': True},
+    'volume_type': {'type': 'string', 'allowed': ['gp2', 'io1', 'standard'], 'required' : True},
+    'volume_size': {'type': 'integer', 'required': True},
+    'iops': {'type': 'integer', 'required': False}
+    }}
