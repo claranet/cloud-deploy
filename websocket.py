f77d29c6f035cebf715e994325e432ade2968bfb
Patrick Decat <pdecat@morea.fr>
2015-11-26 17:27:42 +0100

    GHOST-199: Fix: spawn the follow loop in another thread to end this request and avoid CLOSED_WAIT connections leaking

--- websocket.py
+++ websocket.py
@@ -1,4 +1,5 @@
-from flask_socketio import SocketIO, emit
+from flask import request
+from flask_socketio import SocketIO
 import gevent
 import re
 
@@ -84,43 +85,72 @@ def ansi_to_html(text):
 def create_ws(app):
     socketio = SocketIO(app)
 
-    def follow(filename, last_pos):
+    def follow(filename, last_pos, sid):
+        print 'SocketIO: starting loop for ' + sid
         try:
             hub = gevent.get_hub()
-            watcher = hub.loop.stat(filename)
-            while True:
+            watcher = hub.loop.stat(str(filename))
+
+            # Loop until the WebSocket client has disconnected
+            while sid in socketio.server.rooms(sid):
+                # Read all lines from log file, beginning at last_pos
                 lines = []
+                new_pos = 0
                 with open(filename) as f:
                     f.seek(last_pos)
                     for line in f:
                         lines.append(ansi_to_html(line).replace('\n', '<br>'))
-                    last_pos = f.tell()
-                data = {
-                        'html': ''.join(lines),
-                        'last_pos': last_pos,
-                        }
-                emit('job', data)
-                hub.wait(watcher)
+                    new_pos = f.tell()
+
+                # Send new data to WebSocket client, if any
+                if new_pos != last_pos:
+                    data = {
+                            'html': ''.join(lines),
+                            'last_pos': last_pos,
+                            }
+                    socketio.emit('job', data, room=sid)
+                    print 'SocketIO: sent data for ' + sid
+
+                # Update last_pos for next iteration
+                last_pos = new_pos
+
+                # Wait until the log file is modified or a 60 seconds timeout elapses 
+                try:
+                    with gevent.Timeout(60):
+                        hub.wait(watcher)
+                except gevent.Timeout:
+                    continue
         except IOError:
             data = {
                     'html': 'ERROR: failed to read log file.',
                     'last_pos': 0,
                     }
-            emit('job', data)
+            socketio.emit('job', data, room=sid)
+        print 'SocketIO: ending loop for ' + sid
+
+    @socketio.on('connect')
+    def handle_connect():
+        print 'SocketIO: connected from ' + request.sid
+
+    @socketio.on('disconnect')
+    def handle_disconnect():
+        print 'SocketIO: disconnected from ' + request.sid
 
     @socketio.on('job_logging')
     def handle_message(data):
+        print 'SocketIO: request from ' + request.sid
         if data and data.get('log_id'):
             log_id = data.get('log_id')
             last_pos = data.get('last_pos', 0)
             filename = LOG_ROOT + '/' + log_id + '.txt'
-            filename = filename.encode('ascii', 'ignore')
-            follow(filename, last_pos)
+
+            # Spawn the follow loop in another thread to end this request and avoid CLOSED_WAIT connections leaking
+            gevent.spawn(follow, filename, last_pos, request.sid)
         else:
             data = {
                 'html': 'No log file yet.',
                 'last_pos': 0,
                 }
-            emit('job', data)
+            socketio.emit('job', data, room=request.sid)
 
     return socketio
