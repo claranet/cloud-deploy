643d65a70754e41d60ad9898e9f2c76e6d0d6bc2
Patrick Decat <pdecat@morea.fr>
2015-12-21 18:16:34 +0100

    GHOST-159: handle lifecycle hook script cleanup when emptied

--- commands/updatelifecyclehooks.py
+++ commands/updatelifecyclehooks.py
@@ -4,7 +4,7 @@ import traceback
 
 from boto import s3
 
-from commands.tools import refresh_stage2
+from commands.tools import log, refresh_stage2
 
 class Updatelifecyclehooks():
     _app = None
@@ -20,30 +20,37 @@ class Updatelifecyclehooks():
         self._config = worker._config
         self._worker = worker
 
+
+    def _refresh_lifecycle_hook_script(self, lifecycle_hook, lifecycle_hooks, bucket, prefix):
+        key_name = '{prefix}/{lifecycle_hook}'.format(prefix=prefix, lifecycle_hook=lifecycle_hook)
+        lifecycle_hook_source = lifecycle_hooks is not None and lifecycle_hooks.get(lifecycle_hook, None)
+        if lifecycle_hook_source:
+            lifecycle_hook_source = base64.b64decode(lifecycle_hook_source)
+            k = bucket.new_key(key_name)
+            k.set_contents_from_string(lifecycle_hook_source)
+            k.close()
+            log('INFO: uploaded {key}'.format(key=key_name), self._log_file)
+        else:
+            bucket.delete_key(key_name)
+            log('INFO: deleted {key}'.format(key=key_name), self._log_file)
+
+
     def execute(self):
         try:
             app = self._app
             bucket = self._config['bucket_s3']
             refresh_stage2(bucket, app['region'], self._config['ghost_root_path'])
+            log('INFO: refreshed /ghost/stage2', self._log_file)
 
             # Store lifecycle hooks scripts in S3
             lifecycle_hooks = app.get('lifecycle_hooks', None)
-            if lifecycle_hooks is not None:
-                conn = s3.connect_to_region(app['region'])
-                bucket = conn.get_bucket(bucket)
-
-                prefix = '/ghost/{app}/{env}/{role}'.format(app=app['name'], env=app['env'], role=app['role'])
-                if 'pre_bootstrap' in lifecycle_hooks:
-                    pre_bootstrap_source = base64.b64decode(lifecycle_hooks['pre_bootstrap'])
-                    k = bucket.new_key('{prefix}/pre_bootstrap'.format(prefix=prefix))
-                    k.set_contents_from_string(pre_bootstrap_source)
-
-                if 'post_bootstrap' in lifecycle_hooks:
-                    post_bootstrap_source = base64.b64decode(lifecycle_hooks['post_bootstrap'])
-                    k = bucket.new_key('{prefix}/post_bootstrap'.format(prefix=prefix))
-                    k.set_contents_from_string(post_bootstrap_source)
+            conn = s3.connect_to_region(app['region'])
+            bucket = conn.get_bucket(bucket)
+            prefix = '/ghost/{app}/{env}/{role}'.format(app=app['name'], env=app['env'], role=app['role'])
+            self._refresh_lifecycle_hook_script('pre_bootstrap', lifecycle_hooks, bucket, prefix)
+            self._refresh_lifecycle_hook_script('post_bootstrap', lifecycle_hooks, bucket, prefix)
 
             self._worker.update_status("done", message="Scripts Update OK")
         except:
             traceback.print_exc(self._log_file)
-            self._worker.update_status("failed", message="Scripts Update Failed: {1}".format(str(sys.exc_info()[1])))
+            self._worker.update_status("failed", message="Scripts Update Failed: {0}".format(str(sys.exc_info()[1])))
