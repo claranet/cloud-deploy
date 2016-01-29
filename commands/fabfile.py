ad423309e4b354fa829eec1c6b8da086758ac87d
Patrick Decat <pdecat@morea.fr>
2016-01-29 19:19:35 +0100

    GHOST-164: fix fabfile after conflict resolution during rebase

--- commands/fabfile.py
+++ commands/fabfile.py
@@ -20,4 +20,4 @@ def deploy(module, ssh_username, key_filename, bucket_region, log_file):
         sudo('rm -rvf /tmp/stage2', stdout=log_file)
         put(StringIO(render_stage2(config, bucket_region)), '/tmp/stage2')
         sudo('chmod +x /tmp/stage2', stdout=log_file)
-        sudo('/tmp/stage2 %s' % module, stdout=log_file)
+        sudo('/tmp/stage2 %s' % module['name'], stdout=log_file)
