8a2b46f703a4c4a7ba3bccc31a9d3ef553167221
Patrick Decat <pdecat@morea.fr>
2016-04-11 11:23:00 +0200

    GHOST-219: List EC2 instances types in China region (cn-north-1)

--- aws_data.py
+++ aws_data.py
@@ -7,6 +7,39 @@ with open('aws_data_instance_types.json') as data_file:
 
 instance_types = {}
 
+# Instance type from China (cn-north-1) region are not available like in others
+
+instance_types['cn-north-1'] = {
+    InstanceType(name='t2.micro',   cores='1',  memory='1',    disk='EBS only'),
+    InstanceType(name='t2.small',   cores='1',  memory='2',    disk='EBS only'),
+    InstanceType(name='t2.medium',  cores='2',  memory='4',    disk='EBS only'),
+    InstanceType(name='t2.large',   cores='2',  memory='8',    disk='EBS only'),
+    InstanceType(name='m1.small',   cores='1',  memory='1.7',  disk='1 x 160'),
+    InstanceType(name='m3.medium',  cores='1',  memory='3.75', disk='1 x 4 SSD'),
+    InstanceType(name='m3.large',   cores='2',  memory='7.5',  disk='1 x 32 SSD'),
+    InstanceType(name='m3.xlarge',  cores='4',  memory='15',   disk='2 x 40 SSD'),
+    InstanceType(name='m3.2xlarge', cores='8',  memory='30',   disk='2 x 80 SSD'),
+    InstanceType(name='c4.large',   cores='2',  memory='3.75', disk='EBS only'),
+    InstanceType(name='c4.xlarge',  cores='4',  memory='7.5',  disk='EBS only'),
+    InstanceType(name='c4.2xlarge', cores='8',  memory='15',   disk='EBS only'),
+    InstanceType(name='c4.4xlarge', cores='16', memory='30',   disk='EBS only'),
+    InstanceType(name='c4.8xlarge', cores='36', memory='60',   disk='EBS only'),
+    InstanceType(name='c3.large',   cores='2',  memory='3.75', disk='2 x 16 SSD'),
+    InstanceType(name='c3.xlarge',  cores='4',  memory='7.5',  disk='2 x 40 SSD'),
+    InstanceType(name='c3.2xlarge', cores='8',  memory='15',   disk='2 x 80 SSD'),
+    InstanceType(name='c3.4xlarge', cores='16', memory='30',   disk='2 x 160 SSD'),
+    InstanceType(name='c3.8xlarge', cores='32', memory='60',   disk='2 x 320 SSD'),
+    InstanceType(name='r3.large',   cores='2',  memory='15',   disk='1 x 32 SSD'),
+    InstanceType(name='r3.xlarge',  cores='4',  memory='30.5', disk='1 x 80 SSD'),
+    InstanceType(name='r3.2xlarge', cores='8',  memory='61',   disk='1 x 160 SSD'),
+    InstanceType(name='r3.4xlarge', cores='16', memory='122',  disk='1 x 320 SSD'),
+    InstanceType(name='r3.8xlarge', cores='32', memory='244',  disk='2 x 320 SSD'),
+    InstanceType(name='i2.xlarge',  cores='4',  memory='30.5', disk='1 x 800 SSD'),
+    InstanceType(name='i2.2xlarge', cores='8',  memory='61',   disk='2 x 800 SSD'),
+    InstanceType(name='i2.4xlarge', cores='16', memory='122',  disk='4 x 800 SSD'),
+    InstanceType(name='i2.8xlarge', cores='32', memory='244',  disk='8 x 800 SSD'),
+}
+
 for region_data in data:
     region = region_data['region']
     instance_types[region] = []
