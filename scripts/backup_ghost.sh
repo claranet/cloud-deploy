03c975723b01f37d490b52e6aa64242af66e3688
Patrick Decat <pdecat@morea.fr>
2016-04-15 16:55:20 +0200

    GHOST-224: make backup script runnable as ghost user

--- scripts/backup_ghost.sh
+++ scripts/backup_ghost.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
 
-#requirements
-# TODO
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
 
+# Open STDOUT as $LOG_FILE file for read and write.
+LOG_FILE=/var/log/ghost/ghost-backup.log
+exec 1<>$LOG_FILE
+
+# Redirect STDERR to STDOUT
+exec 2>&1
+
+# Variables
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 CONFIG="$SCRIPTPATH/../config.yml"
+S3=$(grep bucket_s3 $CONFIG | cut -d' ' -f2)
+S3_REGION=$(grep bucket_region $CONFIG | cut -d' ' -f2)
+BACKUP=/tmp/backup_ghost
+FILE="ghost-backup-`date +"%F_%H_%M"`.tar.gz"
+
 if [ $# -eq 1 ]; then
     S3_REGION=$1
-elif [ ! -z "$(grep bucket_region $CONFIG )" ]; then
-    S3_REGION=`grep bucket_region $CONFIG | cut -d' ' -f2`
-else
+elif [ -z "$S3_REGION" ]; then
     S3_REGION='eu-west-1'
 fi
 
-BACKUP=/tmp/backup_ghost
-FILE="ghost-backup-`date +"%F_%H_%M"`.tar.gz"
-sudo mkdir $BACKUP
+# Create backup dir
+mkdir $BACKUP
 cd $BACKUP
-sudo mongodump
-sudo cp /home/admin/ghost/*.yml .
-sudo mkdir logs
-sudo cp -r /var/log/ghost* logs/
-cd ..
 
-sudo tar zcf $FILE $BACKUP
-S3=`grep bucket_s3 $CONFIG | cut -d' ' -f2`
+# Dump mongodb
+mongodump
+
+# Copy configuration
+cp /home/admin/ghost/*.yml .
+
+# Copy jobs' logs
+mkdir logs
+cp -r /var/log/ghost/*.txt logs/
+
+# Create archive
+cd ..
+tar -czf $FILE $BACKUP
+
+# Upload archive to S3
 /usr/local/bin/aws s3 cp $FILE --region $S3_REGION s3://$S3/backup/
-sudo rm -rf $BACKUP
-sudo rm -rf $FILE
+
+# Cleanup
+rm -rf $BACKUP
+rm -rf $FILE
