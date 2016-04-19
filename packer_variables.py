076aee4eccced8b2fcd14f31aeff3d39464cf282
Eugène NGONTANG(nixmind) <engontang@morea.fr>
2016-04-19 15:50:26 +0000

    Fix syntax errors in packer_variables module

--- packer_variables.py
+++ packer_variables.py
@@ -7,13 +7,13 @@ class PackerVar:
     def __init__(self):
         self._var_file = VAR_FILE
 
-    def create_vars(var_list):
+    def create_vars(self, var_list):
         with file(self._var_file, 'w') as var_file:
             json.dump(var_list, var_file, indent=4, separators=(',', ': '))
 
-    def delete_vars():
+    def delete_vars(self):
         os.remove(self._var_file)
 
-    def get_var_file():
+    def get_var_file(self):
         return(self._var_file)
 
