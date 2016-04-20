1adaafac86a0776d1737366fcda995d152c11284
Jérôme Respaut <jerome.respaut@fr.clara.net>
2016-04-20 16:28:01 +0200

    GHOST-171: Install only curl

--- pypacker.py
+++ pypacker.py
@@ -82,7 +82,7 @@ class Packer:
             'type': 'shell',
             'inline':[
                 "sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' update",
-                "sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install curl ntp"
+                "sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install curl"
             ]
         },
         {
