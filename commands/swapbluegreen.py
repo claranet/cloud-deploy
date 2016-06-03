64504f93b4676016a44d951ec2a0b1b984a1fa79
Jérôme Respaut <jerome.respaut@fr.clara.net>
2016-06-03 14:24:14 +0200

    GHOST-229: Update swap command and common functions

--- commands/swapbluegreen.py
+++ commands/swapbluegreen.py
@@ -4,7 +4,7 @@ from ghost_log import log
 from ghost_tools import get_aws_connection_data, get_app_friendly_name
 from ghost_aws import check_autoscale_exists
 from settings import cloud_connections, DEFAULT_PROVIDER
-from libs.blue_green import get_blue_green_apps
+from libs.blue_green import get_blue_green_apps, check_app_manifest
 
 COMMAND_DESCRIPTION = "Swap the Blue/Green env"
 
@@ -46,19 +46,27 @@ class Swapbluegreen():
 
     def execute(self):
         log(_green("STATE: Started"), self._log_file)
+        swap_execution_strategy = self._job['options'][0] if 'options' in self._job and len(self._job['options']) > 0 else None
         online_app, to_deploy_app = get_blue_green_apps(self._app,
                                                         self._worker._db.apps)
         if not online_app:
             self._worker.update_status("aborted", message=self._get_notification_message_aborted(self._app, "Blue/green is not enabled on this app or not well configured"))
             return
         try:
+            # Check AMI
             if not to_deploy_app['ami']:
                 self._worker.update_status("aborted", message=self._get_notification_message_aborted(to_deploy_app, "Please run `Buildimage` first"))
                 return
+            # Check ASG
             if not (check_autoscale_exists(self._cloud_connection, to_deploy_app['autoscale']['name'], to_deploy_app['region'])
                 and check_autoscale_exists(self._cloud_connection, online_app['autoscale']['name'], online_app['region'])):
                 self._worker.update_status("aborted", message=self._get_notification_message_aborted(to_deploy_app, "Please set an AutoScale on both green and blue app"))
                 return
+            # Check if modules have been deployed
+            if not check_app_manifest(to_deploy_app, self._config, self._log_file):
+                self._worker.update_status("aborted", message=self._get_notification_message_aborted(to_deploy_app, "Please deploy your app's modules"))
+                return
+            # Check if we're ready to swap
             self._worker.update_status("done")#, message=self._get_notification_message_done(online_app, ))
         except GCallException as e:
             self._worker.update_status("failed", message=self._get_notification_message_failed(online_app, to_deploy_app, e))
--- libs/blue_green.py
+++ libs/blue_green.py
@@ -1,5 +1,9 @@
 """Library pertaining to blue/green commands."""
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
+import sys
+from libs.deploy import get_path_from_app_with_color
 
 def get_blue_green_apps(app, apps_db):
     """
@@ -20,3 +24,18 @@ def get_blue_green_apps(app, apps_db):
                 return None, None
     else:
         return None, None
+
+def check_app_manifest(self, app, config, log_file):
+    key_path = get_path_from_app_with_color(app) + '/MANIFEST'
+    cloud_connection = cloud_connections.get(app.get('provider', DEFAULT_PROVIDER))(log_file)
+    conn = cloud_connection.get_connection(config.get('bucket_region', app['region']), ["s3"])
+    bucket = conn.get_bucket(config['bucket_s3'])
+    key = bucket.get_key(key_path)
+    if not key:
+        return False
+    manifest = key.get_contents_as_string()
+    if sys.version > '3':
+        manifest = manifest.decode('utf-8')
+    nb_deployed_modules = len(manifest.split('\n')):
+    nb_app_modules = len(app['modules'])
+    return nb_deployed_modules == nb_app_modules
