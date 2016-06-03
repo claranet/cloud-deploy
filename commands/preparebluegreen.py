4773cea98f8f112006b45b009b510cc7f2fcd788
Adrien Bréfort <adrien.brefort@fr.clara.net>
2016-06-03 10:33:10 +0200

    Check if blue/green is enabled.

--- commands/preparebluegreen.py
+++ commands/preparebluegreen.py
@@ -1,15 +1,20 @@
+"""Classes pertaining to blue/green preparation."""
 from fabric.colors import green as _green, yellow as _yellow, red as _red
 
 from ghost_log import log
 
 COMMAND_DESCRIPTION = "Prepare the Blue/Green env before swap"
 
-class Preparebluegreen():
+
+class Preparebluegreen(object):
+    """Checks and prepares blue/green deployment before swap."""
+
     _app = None
     _job = None
     _log_file = -1
 
     def __init__(self, worker):
+        """init from worker attributes."""
         self._app = worker.app
         self._job = worker.job
         self._db = worker._db
@@ -17,9 +22,24 @@ class Preparebluegreen():
         self._worker = worker
         self._log_file = worker.log_file
 
+    def _check_blue_green_enabled(self):
+        """Check if blue/green deployment is enabled for this app."""
+        if not self._app.get('blue_green', None):
+            message = 'blue/green is not enabled for this app'
+            log(_red('ERROR: ' + message),
+                self._log_file)
+            self._worker.update_status("failed", message=message)
+            return False
+        return True
+
     def execute(self):
+        """Execute all checks and preparations."""
         log(_green("STATE: Started"), self._log_file)
+
         # Check if app has blue green enabled, if is not online
+        if not self._check_blue_green_enabled():
+            return
+
         # Check if app has AS
         # Check if app has up to date AMI
         # Check if all module have been deployed
