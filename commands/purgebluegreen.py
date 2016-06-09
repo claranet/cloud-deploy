141658bd2ebdf09d50ccc29e053f6159bfcaf1fd
Jérôme Respaut <jerome.respaut@fr.clara.net>
2016-06-09 14:51:39 +0200

    GHOST-229: Purge command update app autoscale values and suspend ASG to prevent scaling during the process

--- commands/purgebluegreen.py
+++ commands/purgebluegreen.py
@@ -4,7 +4,7 @@ from ghost_log import log
 from ghost_tools import get_aws_connection_data, get_app_friendly_name, GCallException
 from settings import cloud_connections, DEFAULT_PROVIDER
 
-from ghost_aws import check_autoscale_exists
+from ghost_aws import check_autoscale_exists, get_autoscaling_group_and_processes_to_suspend, suspend_autoscaling_group_processes, resume_autoscaling_group_processes
 from libs.elb import get_elb_instance_status_autoscaling_group, get_elb_from_autoscale, destroy_elb, register_elb_into_autoscale
 from libs.autoscaling import get_instances_from_autoscaling, flush_instances_update_autoscale
 from libs.blue_green import get_blue_green_apps
@@ -45,6 +45,16 @@ class Purgebluegreen():
         notif = "Blue/green purge done for [{0}] [{1}]".format(get_app_friendly_name(app), app['_id'])
         return _green(notif)
 
+    def _update_app_autoscale_options(self, app, log_file):
+        """ Updates the App DB object to set the 'is_online' attribute. This attribute should be at True when the ASG is mapped with the online ELB.
+        """
+        self._worker._db.apps.update({ '_id': app['_id']}, {'$set': {
+            'autoscale.min': 0,
+            'autoscale.max': 0,
+            'autoscale.current': 0
+        }})
+        log(_green("'{0}' autoscale has been update '{1}'".format(app['_id'], app['autoscale']['name'])), log_file)
+
     def execute(self):
         log(_green("STATE: Started"), self._log_file)
         online_app, offline_app = get_blue_green_apps(self._app,
@@ -52,25 +62,30 @@ class Purgebluegreen():
         if not offline_app:
             self._worker.update_status("aborted", message=self._get_notification_message_aborted(self._app, "Blue/green is not enabled on this app or not well configured"))
             return
-        try:
-            # Check ASG
-            if offline_app['autoscale']['name'] and online_app['autoscale']['name']:
-                if not (check_autoscale_exists(self._cloud_connection, offline_app['autoscale']['name'], offline_app['region'])
-                    and check_autoscale_exists(self._cloud_connection, online_app['autoscale']['name'], online_app['region'])):
-                    self._worker.update_status("aborted", message=self._get_notification_message_aborted(offline_app, "Not AutoScale group found on the offline app to purge."))
-                    return
-            else:
+
+        # Check ASG
+        if offline_app['autoscale']['name'] and online_app['autoscale']['name']:
+            if not (check_autoscale_exists(self._cloud_connection, offline_app['autoscale']['name'], offline_app['region'])
+                and check_autoscale_exists(self._cloud_connection, online_app['autoscale']['name'], online_app['region'])):
                 self._worker.update_status("aborted", message=self._get_notification_message_aborted(offline_app, "Not AutoScale group found on the offline app to purge."))
                 return
+        else:
+            self._worker.update_status("aborted", message=self._get_notification_message_aborted(offline_app, "Not AutoScale group found on the offline app to purge."))
+            return
 
-            app_region = offline_app['region']
+        # Retrieve autoscaling infos, if any
+        app_region = offline_app['region']
+        as_conn = self._cloud_connection.get_connection(app_region, ["ec2", "autoscale"])
+        as_group, as_group_processes_to_suspend = get_autoscaling_group_and_processes_to_suspend(as_conn, offline_app, self._log_file)
+        suspend_autoscaling_group_processes(as_conn, as_group, as_group_processes_to_suspend, self._log_file)
+
+        try:
             as_conn3 = self._cloud_connection.get_connection(app_region, ['autoscaling'], boto_version='boto3')
             # Check if instances are running
             if not get_instances_from_autoscaling(offline_app['autoscale']['name'], as_conn3):
                 self._worker.update_status("aborted", message=self._get_notification_message_aborted(offline_app, "Autoscaling Group of offline app is empty. Nothing to do"))
                 return
 
-            as_conn = self._cloud_connection.get_connection(app_region, ["ec2", "autoscale"])
             ec2_conn = self._cloud_connection.get_connection(app_region, ["ec2"])
             elb_conn3 = self._cloud_connection.get_connection(app_region, ['elb'], boto_version='boto3')
             temp_elbs = get_elb_from_autoscale(offline_app['autoscale']['name'], as_conn)
@@ -80,16 +95,20 @@ class Purgebluegreen():
                 return
 
             # Detach temp ELB from ASG
-            log("Detach the current temporary ELB [{0}] from the AutoScale [{1}]".format(temp_elbs, offline_app['autoscale']['name']), self._log_file)
+            log(_green("Detach the current temporary ELB [{0}] from the AutoScale [{1}]".format(temp_elbs, offline_app['autoscale']['name'])), self._log_file)
             register_elb_into_autoscale(offline_app['autoscale']['name'], as_conn3, temp_elbs, None, self._log_file)
             # Update ASG and kill instances
             log("Update AutoScale with `0` on mix, max, desired values.", self._log_file)
-            log("Destroy all instances in the AutoScale and all instances matching the `app_id` [{0}]".format(offline_app['_id']), self._log_file)
+            log(_yellow("Destroy all instances in the AutoScale and all instances matching the `app_id` [{0}]".format(offline_app['_id'])), self._log_file)
             flush_instances_update_autoscale(as_conn, ec2_conn, offline_app, self._log_file)
             # Destroy temp ELB
             destroy_elb(elb_conn3, temp_elbs[0], self._log_file)
-
+            # Update App Autoscale values, next buildimage or updateautoscaling should not set values different from 0
+            self._update_app_autoscale_options(offline_app, self._log_file)
             # All good
             self._worker.update_status("done", message=self._get_notification_message_done(offline_app))
         except GCallException as e:
             self._worker.update_status("failed", message=self._get_notification_message_failed(offline_app, str(e)))
+        finally:
+            # Resume autoscaling groups in any case
+            resume_autoscaling_group_processes(as_conn, as_group, as_group_processes_to_suspend, self._log_file)
