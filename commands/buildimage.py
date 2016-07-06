f1cd4c2bc69e6495dd93e17035586a29e3aa3910
Jérôme Respaut <jerome.respaut@fr.clara.net>
2016-07-06 11:34:46 +0200

    Hotfix GHOST-253 empty hook should be unicode

--- commands/buildimage.py
+++ commands/buildimage.py
@@ -165,7 +165,7 @@ class Buildimage():
         log("Create '%s' script for Packer" % hook_name, self._log_file)
         lfc_hooks = self._app.get('lifecycle_hooks', None)
         if not lfc_hooks or not lfc_hooks.get(hook_name, None):
-            hook_source = ''
+            hook_source = u''
         else:
             hook_source = b64decode_utf8(self._app['lifecycle_hooks'][hook_name])
         app_path = "/ghost/{name}/{env}/{role}".format(name=self._app['name'], env=self._app['env'], role=self._app['role'])
