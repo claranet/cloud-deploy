cd95980e929a4a32bfb7f9106f0b50359b4bc438
Patrick Decat <pdecat@morea.fr>
2016-07-08 11:47:20 +0200

    Hotfix GHOST-253 empty hook breaks zsh

--- commands/buildimage.py
+++ commands/buildimage.py
@@ -163,10 +163,36 @@ class Buildimage():
             self._worker.update_status("done")
 
     def _generate_buildimage_hook(self, hook_name):
+        """ Generates a buildimage hook script
+        >>> from StringIO import StringIO
+        >>> from ghost_tools import b64encode_utf8
+        >>> class worker:
+        ...     app = { \
+                    'name': 'AppName', 'env': 'prod', 'role': 'webfront', 'region': 'eu-west-1',\
+                    'lifecycle_hooks': {'pre_buildimage': u'', 'post_buildimage': b64encode_utf8(u'echo Custom post-buildimage script')}\
+                 }
+        ...     job = None
+        ...     log_file = StringIO()
+        ...     _config = None
+        ...     _db = None
+
+        >>> Buildimage(worker=worker())._generate_buildimage_hook('pre_buildimage')
+        '/ghost/AppName/prod/webfront/hook-pre_buildimage'
+        >>> with io.open('/ghost/AppName/prod/webfront/hook-pre_buildimage', encoding='utf-8') as f:
+        ...   f.read()
+        u'echo No pre_buildimage script'
+
+        >>> Buildimage(worker=worker())._generate_buildimage_hook('post_buildimage')
+        '/ghost/AppName/prod/webfront/hook-post_buildimage'
+        >>> with io.open('/ghost/AppName/prod/webfront/hook-post_buildimage', encoding='utf-8') as f:
+        ...   f.read()
+        u'echo Custom post-buildimage script'
+        """
+        pillar = {}
         log("Create '%s' script for Packer" % hook_name, self._log_file)
         lfc_hooks = self._app.get('lifecycle_hooks', None)
         if not lfc_hooks or not lfc_hooks.get(hook_name, None):
-            hook_source = u''
+            hook_source = u"echo No {hook_name} script".format(hook_name=hook_name)
         else:
             hook_source = b64decode_utf8(self._app['lifecycle_hooks'][hook_name])
         app_path = "/ghost/{name}/{env}/{role}".format(name=self._app['name'], env=self._app['env'], role=self._app['role'])
