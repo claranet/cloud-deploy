a8cb243a04516deda1d6ec73b186c0fc73e389e0
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-01-31 12:01:26 +0100

    GHOST-7: tox TU updates

--- command.py
+++ command.py
@@ -22,13 +22,15 @@ def format_notif(app, job):
     """
     Returns a formatted title and message couple
 
-    >>> app = {'name': 'myapp', 'env': 'preprod'}
+    >>> app = {'name': 'myapp', 'env': 'preprod', 'role': 'webfront'}
     >>> job = {'command': 'deploy', 'user': 'john', '_created': '2015-06-10 17:09:38', 'status': 'done', 'message': 'Deployment OK: [mymodule]'}
-    >>> title, message = format_notif(app, job)
+    >>> title, message, slack_msg = format_notif(app, job)
     >>> title
     '[GHOST] App myapp (preprod) - deploy: done (Deployment OK: [mymodule])'
     >>> message
     'Application: myapp\\nEnvironment: preprod\\nAction: deploy\\nStarted: 2015-06-10 17:09:38\\nStatus: done\\nUser: john\\nMessage: Deployment OK: [mymodule]'
+    >>> slack_msg
+    ':ok_hand: [myapp][*preprod*][webfront] *deploy* job triggered by [*john*] is *done* (message: Deployment OK: [mymodule])'
     """
     title_template = "[GHOST] App {app_name} ({app_env}) - {command}: {status} ({message})"
     title = title_template.format(app_name=app['name'],
