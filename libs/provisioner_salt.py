4b8b946fb53ee28c43252591b187c3b20063acc7
sjeoffroi <sjeoffroi@morea.fr>
2017-06-20 16:53:03 +0200

    GHOST-374 refactor salt provisioner as ansible provisioner

--- libs/provisioner_salt.py
+++ libs/provisioner_salt.py
@@ -1,16 +1,25 @@
 import os
+import copy
 import re
 import yaml
 from fabric.colors import yellow as _yellow
 
 from ghost_log import log
-from ghost_tools import config
 
 from .provisioner import FeaturesProvisioner
+
+SALT_PILLAR_TOP = {'base': {'*': ['features']}}
 
 class FeaturesProvisionerSalt(FeaturesProvisioner):
     def __init__(self, log_file, unique_id, config, global_config):
         FeaturesProvisioner.__init__(self, log_file, 'salt', unique_id, config, global_config)
+        self._salt_state_tree = os.path.join(self.local_repo_path, 'salt')
+        self._salt_pillar_roots = os.path.join(self.local_repo_path, 'pillar')
+        self._provisioner_log_level = self.global_config.get('provisioner_log_level', 'info')
+        self._salt_state_top_path = os.path.join(self._salt_state_tree, 'top.sls')
+        self._salt_pillar_top_path = os.path.join(self._salt_pillar_roots, 'top.sls')
+        self._salt_pillar_features_path = os.path.join(self._salt_pillar_roots, 'features.sls')
+        self._salt_additional_pillar = self.global_config.get('salt_additional_pillar', '')
 
     def build_provisioner_features_files(self, params, features):
         """ Build salt files only if features with salt provisioner """
@@ -20,12 +29,11 @@ class FeaturesProvisionerSalt(FeaturesProvisioner):
             self._build_salt_pillar(params)
 
     def build_packer_provisioner_config(self, packer_config):
-        self._provisioner_log_level = config.get('provisioner_log_level', 'info')
         if self._enabled_packer_salt_config:
             return [{
                 'type': 'salt-masterless',
-                'local_state_tree': self.local_repo_path + '/salt',
-                'local_pillar_roots': self.local_repo_path + '/pillar',
+                'local_state_tree': self._salt_state_tree,
+                'local_pillar_roots': self._salt_pillar_roots,
                 'skip_bootstrap': packer_config['skip_provisioner_bootstrap'],
                 'log_level': self._provisioner_log_level,
             }]
@@ -49,61 +57,54 @@ class FeaturesProvisionerSalt(FeaturesProvisioner):
 
         >>> features = []
         >>> import pprint
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None)._test_not_empty_salt_features(features))
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {})._test_not_empty_salt_features(features))
         False
 
         >>> features = ['pkg']
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None)._test_not_empty_salt_features(features))
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {})._test_not_empty_salt_features(features))
         True
 
         """
         return features != []
 
     def _build_salt_top(self, params):
-        self.salt_path = self.local_repo_path + '/salt'
-        self.salt_top_path = self.salt_path + '/top.sls'
-        stream = file(self.salt_top_path, 'w')
-        log("Salt - Writing Top state to: {0}".format(self.salt_top_path), self._log_file)
-        #The common sls file is optional
-        if os.path.exists(self.salt_path + '/common'):
-            data = {'base': {'*': ['common'] + params}}
-        else:
-            data = {'base': {'*': params}}
-        log('Salt - state: top.sls: {0}'.format(data), self._log_file)
-        yaml.dump(data, stream, default_flow_style=False)
+        """ Build salt salt/top.sls file from features """
+        with open(self._salt_state_top_path, "w") as stream:
+            log("Salt - Writing Top state to: {0}".format(self._salt_state_top_path), self._log_file)
+            if os.path.exists(os.path.join(self._salt_state_tree, 'common')):
+                data = {'base': {'*': ['common'] + params}}
+            else:
+                data = {'base': {'*': params}}
+            log('Salt - state: top.sls: {0}'.format(data), self._log_file)
+            yaml.dump(data, stream, default_flow_style=False)
 
     def _build_salt_pillar(self, features):
-        self.salt_pillar_path = self.local_repo_path + '/pillar'
-        self.salt_pillar_top_path = self.salt_pillar_path + '/top.sls'
-        self.salt_pillar_features_path = self.salt_pillar_path + '/features.sls'
-        self._salt_additional_pillar = config.get('salt_additional_pillar', '')
-        #Creating top.sls to call features.sls
-        stream_top = file(self.salt_pillar_top_path, 'w')
-        data_top = {'base': {'*': ['features']}}
-        if self._salt_additional_pillar != '':
-            data_top['base']['*'].append(self._salt_additional_pillar)
-        else:
-            log('Salt - No additional pillar to add', self._log_file)
-        log('Salt - pillar: top.sls: {0}'.format(data_top), self._log_file)
-        yaml.dump(data_top, stream_top, default_flow_style=False)
-        #Creating features.sls file based on ghost app features
-        stream_features = file(self.salt_pillar_features_path, 'w')
-        log(_yellow('Salt - pillar: features.sls: {0}'.format(features)), self._log_file)
-        yaml.dump(features, stream_features, default_flow_style=False)
+        """ Build salt pillar/top.sls and pillar/features.sls """
+        data_top = copy.deepcopy(SALT_PILLAR_TOP)
+        with open(self._salt_pillar_top_path, "w") as stream_top:
+            if self._salt_additional_pillar != '':
+                data_top['base']['*'].append(self._salt_additional_pillar)
+            else:
+                log('Salt - No additional pillar to add', self._log_file)
+            log('Salt - pillar: top.sls: {0}'.format(data_top), self._log_file)
+            yaml.dump(data_top, stream_top, default_flow_style=False)
+        with open(self._salt_pillar_features_path, "w") as stream_features:
+            log(_yellow('Salt - pillar: features.sls: {0}'.format(features)), self._log_file)
+            yaml.dump(features, stream_features, default_flow_style=False)
 
     def format_provisioner_features(self, features):
         """ Generates the formula dictionnary object with all required features
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim'}, {'name': 'pkg', 'version': 'package=lsof'}, {'name': 'pkg', 'version': 'package=curl'}]
-        >>> FeaturesProvisionerSalt(None, None, None, None).format_provisioner_features(features)
+        >>> FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_features(features)
         ['pkg']
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim', 'provisioner': 'salt'}, {'name': 'pkg', 'version': 'package=lsof', 'provisioner': 'salt'}, {'name': 'pkg', 'version': 'package=curl', 'provisioner': 'salt'}]
-        >>> FeaturesProvisionerSalt(None, None, None, None).format_provisioner_features(features)
+        >>> FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_features(features)
         ['pkg']
 
         >>> features = []
-        >>> FeaturesProvisionerSalt(None, None, None, None).format_provisioner_features(features)
+        >>> FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_features(features)
         []
 
         """
@@ -126,23 +127,23 @@ class FeaturesProvisionerSalt(FeaturesProvisioner):
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim'}, {'name': 'pkg', 'version': 'package=lsof'}, {'name': 'pkg', 'version': 'package=curl'}]
         >>> import pprint
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None).format_provisioner_params(features).items())
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_params(features).items())
         [('pkg', {'package': ['lsof', 'curl'], 'version': 'git_vim'})]
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim', 'provisioner': 'salt'}, {'name': 'pkg', 'version': 'package=lsof', 'provisioner': 'salt'}, {'name': 'pkg', 'version': 'package=curl', 'provisioner': 'salt'}]
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None).format_provisioner_params(features).items())
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_params(features).items())
         [('pkg', {'package': ['lsof', 'curl'], 'version': 'git_vim'})]
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim', 'provisioner': 'ansible'}, {'name': 'pkg', 'version': 'package=lsof', 'provisioner': 'salt'}, {'name': 'pkg', 'version': 'package=curl', 'provisioner': 'salt'}]
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None).format_provisioner_params(features).items())
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_params(features).items())
         [('pkg', {'package': ['lsof', 'curl']})]
 
         >>> features = [{'name': 'pkg', 'version': 'git_vim', 'provisioner': 'ansible'}, {'name': 'pkg', 'version': 'package=lsof', 'provisioner': 'ansible'}, {'name': 'pkg', 'version': 'package=curl', 'provisioner': 'ansible'}]
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None).format_provisioner_params(features).items())
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_params(features).items())
         []
 
         >>> features = []
-        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, None, None).format_provisioner_params(features).items())
+        >>> pprint.pprint(FeaturesProvisionerSalt(None, None, {}, {}).format_provisioner_params(features).items())
         []
 
         """
