4b5e2216fc3c7ae32092c95b2caa9996ec8bb834
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-07-27 12:18:17 +0200

    GHOST-450: Unit test

---
+++ tests/test_elb_mixed.py
@@ -0,0 +1,19 @@
+from mock import MagicMock, mock
+
+from libs.load_balancing import AwsMixedLoadBalancersManager
+from tests.helpers import LOG_FILE, mocked_logger, get_aws_data
+
+
+@mock.patch('libs.load_balancing.log', new=mocked_logger)
+def test_get_instances_status_from_autoscale():
+    cloud_connection = MagicMock()
+    connection = MagicMock()
+    cloud_connection.get_connection.return_value = connection
+
+    connection.describe_load_balancers.return_value = get_aws_data('elbv2--describe-load-balancers')
+    connection.describe_target_groups.return_value = get_aws_data('elbv2--describe-target-groups')
+    connection.describe_load_balancer_target_groups.return_value = get_aws_data('autoscaling--describe-load-balancer-target-groups')
+
+    ret = AwsMixedLoadBalancersManager(cloud_connection, 'region').get_instances_status_from_autoscale('as_test', LOG_FILE)
+
+    assert ret == {}
