5ffa015dfb853f1f5f84d36ec5495eb925651632
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-09-27 10:23:32 +0200

    GHOST-481: Use `/var/lib/ghost` also for `stage2` script at deploy

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+import yaml
+
 from StringIO import StringIO
 from fabric.api import show, sudo, task, env, put, settings, output
 from fabric.context_managers import shell_env
-import yaml
-import os
 
 with open(os.path.dirname(os.path.realpath(__file__)) + '/config.yml', 'r') as conf_file:
     config = yaml.load(conf_file)
@@ -19,22 +20,26 @@ env.colorize_errors = True
 
 output.debug = True
 
+STAGE2_PATH = '/var/lib/ghost/stage2_deploy'
+
+
 @task
-def deploy(module, ssh_username, key_filename, stage2, log_file):
+def deploy(app_module, ssh_username, key_filename, stage2, log_file):
     with settings(show('debug'), warn_only=True, user=ssh_username, key_filename=key_filename):
-        sudo('rm -rvf /tmp/stage2', stdout=log_file)
-        put(StringIO(stage2), '/tmp/stage2')
-        sudo('chmod +x /tmp/stage2', stdout=log_file)
-        result = sudo('/tmp/stage2 %s' % module['name'], stdout=log_file)
+        sudo('rm -rvf {s}'.format(s=STAGE2_PATH), stdout=log_file)
+        put(StringIO(stage2), STAGE2_PATH, use_sudo=True, mode=0754)
+        result = sudo('{s} {n}'.format(s=STAGE2_PATH, n=app_module['name']), stdout=log_file)
         return result.return_code
+
 
 @task
 def executescript(ssh_username, key_filename, context_path, sudoer_user, jobid, hot_script, log_file, ghost_env):
     with settings(show('debug'), warn_only=True, user=ssh_username, key_filename=key_filename):
-        working_dir = '/ghost/%s' % jobid
-        sudo('mkdir -p "%s"' % working_dir, stdout=log_file)
-        put(StringIO(hot_script), '%s/ghost-execute-script' % working_dir, use_sudo=True)
-        sudo('chmod +x %s/ghost-execute-script' % working_dir, stdout=log_file)
+        working_dir = '/ghost/{j}'.format(j=jobid)
+        sudo('mkdir -p "{w}"'.format(w=working_dir), stdout=log_file)
+        put(StringIO(hot_script), '{w}/ghost-execute-script'.format(w=working_dir), use_sudo=True, mode=0754)
         with shell_env(**ghost_env):
-            result = sudo('cd "{c}" && {w}/ghost-execute-script'.format(c=context_path, w=working_dir), stdout=log_file, user=sudoer_user)
+            result = sudo('cd "{c}" && {w}/ghost-execute-script'.format(c=context_path,
+                                                                        w=working_dir),
+                          stdout=log_file, user=sudoer_user)
             return result.return_code
