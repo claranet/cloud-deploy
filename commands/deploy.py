cb05c83da924fde818968d5d71a9a67f6b3273f8
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-10-16 16:51:41 +0200

    GHOST-500: Keep Timestamp and update `_created` deployment field

--- commands/deploy.py
+++ commands/deploy.py
@@ -419,9 +419,7 @@ GHOST_MODULE_USER="{user}"
             log("After all deploy script found for '{0}'. Executing it.".format(module['name']), self._log_file)
             execute_module_script_on_ghost(self._app, module, 'after_all_deploy', 'After all deploy', clone_path, self._log_file)
 
-        # Update ts again for deployment date
         now = datetime.datetime.utcnow()
-        ts = calendar.timegm(now.timetuple())
         deployment = {
             'app_id': self._app['_id'],
             'job_id': self._job['_id'],
@@ -433,5 +431,6 @@ GHOST_MODULE_USER="{user}"
             'package': pkg_name,
             'module_path': module['path'],
             '_created': now,
+            '_updated': now,
         }
         return self._worker._db.deploy_histories.insert(deployment)
