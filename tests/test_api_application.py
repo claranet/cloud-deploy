c357512ca1083b6f07feb95c0babc86744a1b755
Burak Karamahmut <burak.karamahmut@fr.clara.net>
2018-04-27 17:35:08 +0200

    GHOST-600 Renamed file

--- tests/test_api_application.py
+++ tests/test_api_application.py
@@ -2,6 +2,7 @@ import flask
 import mock
 from mock import MagicMock
 from run import pre_insert_app
+from tests.helpers import create_test_app_context
 
 @mock.patch('run.get_apps_db')
 @mock.patch('run.request')
@@ -10,13 +11,7 @@ def test_application_insert(request, get_apps_db):
   apps_db.find_one = MagicMock(return_value=False)
   get_apps_db.return_value = apps_db
 
-  app = flask.Flask(__name__)
-  app.config.update(REQUEST_METHOD='GET', SERVER_NAME='localhost', SERVER_PORT='5000', SECRET_KEY='a random string', WTF_CSRF_SECRET_KEY='a random string')
-  app.config['wsgi.url_scheme'] = 'http'
-  app.app_context().push()
-  request_context = app.request_context(app.config)
-  request_context.request.environ['wsgi.errors'] = None
-  request_context.push()
+  create_test_app_context()
 
   app = {'name': 'name', 'env': 'env', 'role': 'role'}
   pre_insert_app([app])
