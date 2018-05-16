e6443b767d403c0aff0707755741c701206f9c39
Burak Karamahmut <burak.karamahmut@fr.clara.net>
2018-05-16 15:51:49 +0200

    GHOST-600 Improved applicationg testing

--- tests/test_api_application.py
+++ tests/test_api_application.py
@@ -6,7 +6,7 @@ from tests.helpers import create_test_app_context
 
 @mock.patch('run.get_apps_db')
 @mock.patch('run.request')
-def test_application_insert(request, get_apps_db):
+def test_application_pre_insert(request, get_apps_db):
   apps_db = MagicMock()
   apps_db.find_one = MagicMock(return_value=False)
   get_apps_db.return_value = apps_db
@@ -22,4 +22,5 @@ def test_application_insert(request, get_apps_db):
   app = {'name': 'name', 'env': 'env', 'role': 'role', 'modules': modules}
   pre_insert_app([app])
 
-  assert app['modules'][0]['initialized'] == False
+  assert app['modules'] == [{'initialized': False, 'path': '/var/www', 'scope': 'code', 'name': 'test', 'git_repo': 'test'}]
+  assert app['environment_infos'] == {'instance_tags': [{'tag_name': 'Name', 'tag_value': 'ec2.env.role.name'}]}
