6c869611ed211901631186753f4a6face2a9002b
Burak Karamahmut <burak.karamahmut@fr.clara.net>
2018-06-22 12:21:24 +0200

    GHOST-661 Added config to enable LXD, report LXD exception

--- ghost_lxd.py
+++ ghost_lxd.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import jsonify
+from flask import abort, jsonify
 
 from ghost_tools import config
 from libs.lxd import lxd_is_available, list_lxd_images
@@ -10,9 +10,15 @@ lxd_blueprint = Blueprint('lxd_blueprint', __name__)
 
 @lxd_blueprint.route('/lxd/status', methods=['GET'])
 def lxd_status():
-    return jsonify({"status": lxd_is_available(config)})
+  try:
+    return jsonify({'status': lxd_is_available(config)})
+  except Exception as e:
+    return jsonify({'status': False, 'error': type(e).__name__})
 
 
 @lxd_blueprint.route('/lxd/images', methods=['GET'])
 def list_images():
+  try:
     return jsonify(list_lxd_images(config))
+  except Exception as e:
+    return jsonify({'images': [('', 'Container Image list is unavailable, check your LXD parameters in config.yml')], 'error': type(e).__name__})
--- libs/lxd.py
+++ libs/lxd.py
@@ -1,3 +1,5 @@
+import traceback
+
 from pylxd import Client as LXDClient
 
 
@@ -9,19 +11,23 @@ def list_lxd_images(config=None):
     """
     config = config or {}
     if lxd_is_available(config):
-        container_config = config.get('container', {'endpoint': config.get('endpoint',
-                                                                           DEFAULT_LXD_REMOTE_ENDPOINT)})
-        if container_config.get('endpoint', 'localhost') == "localhost":
-            lxd = LXDClient(timeout=container_config.get('timeout', 10))
-        else:
-            lxd = LXDClient(endpoint=container_config.get('endpoint', DEFAULT_LXD_REMOTE_ENDPOINT), verify=True,
-                            timeout=container_config.get('timeout', 10))
-        images = lxd.images.all()
+        try:
+            container_config = config.get('container', {'endpoint': config.get('endpoint',
+                                                                               DEFAULT_LXD_REMOTE_ENDPOINT)})
+            if container_config.get('endpoint', 'localhost') == "localhost":
+                lxd = LXDClient(timeout=container_config.get('timeout', 10))
+            else:
+                lxd = LXDClient(endpoint=container_config.get('endpoint', DEFAULT_LXD_REMOTE_ENDPOINT), verify=True,
+                                timeout=container_config.get('timeout', 10))
+            images = lxd.images.all()
 
-        return [('', "Don't use containers")] + \
-               [(image.fingerprint,
-                 '{} - {}'.format(image.properties.get('description'), ','.join([a['name'] for a in image.aliases])))
-                for image in images]
+            return [('', "Don't use containers")] + \
+                   [(image.fingerprint,
+                     '{} - {}'.format(image.properties.get('description'), ','.join([a['name'] for a in image.aliases])))
+                    for image in images]
+        except:
+            traceback.print_exc()
+            return [('', 'Container Image list is unavailable, check your LXD parameters in config.yml')]
     else:
         return [('', 'Container Image list is unavailable, check your LXD parameters in config.yml')]
 
@@ -31,14 +37,17 @@ def lxd_is_available(config=None):
     Test if lxd is available on system and test if remote lxd endpoint is available
     """
     config = config or {}
-    try:
-        container_config = config.get('container', {'endpoint': config.get('endpoint',
-                                                                           DEFAULT_LXD_REMOTE_ENDPOINT)})
-        lxd_local = LXDClient(timeout=container_config.get('timeout', 10))
+    if config.get('lxd_container_enabled', False):
+        try:
+            container_config = config.get('container', {'endpoint': config.get('endpoint',
+                                                                               DEFAULT_LXD_REMOTE_ENDPOINT)})
+            lxd_local = LXDClient(timeout=container_config.get('timeout', 10))
 
-        if container_config.get('endpoint', 'localhost') != "localhost":
-            lxd = LXDClient(endpoint=container_config.get('endpoint', DEFAULT_LXD_REMOTE_ENDPOINT), verify=True,
-                            timeout=container_config.get('timeout', 10))
-    except:
+            if container_config.get('endpoint', 'localhost') != "localhost":
+                lxd = LXDClient(endpoint=container_config.get('endpoint', DEFAULT_LXD_REMOTE_ENDPOINT), verify=True,
+                                timeout=container_config.get('timeout', 10))
+        except:
+            traceback.print_exc()
+            raise
+    else:
         return False
-    return True
