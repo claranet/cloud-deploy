4c5b24c76a0db819a8b944b4091b192a1030bd6a
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-07-06 11:15:56 +0200

    GHOST-537: Refactor `pypacker.py` lib

--- pypacker.py
+++ pypacker.py
@@ -1,11 +1,9 @@
-import sh
-from subprocess32 import Popen, PIPE
-import json
 import os
 
-from ghost_tools import GCallException
-from ghost_log import log
+from subprocess32 import Popen, PIPE
 
+from ghost_log import log
+from ghost_tools import boolify
 
 PACKER_LOGDIR="/var/log/ghost/packer"
 
@@ -13,24 +11,26 @@ PACKER_LOGDIR="/var/log/ghost/packer"
 class Packer:
     def __init__(self, credentials, log_file):
         self._log_file = log_file
-        credentials = json.dumps(credentials, sort_keys=True, indent=4, separators=(',', ': '))
-        self.packer_config = json.loads(credentials)
-        if self.packer_config['aws_access_key']:
-            self._assumed_role = True
-        else:
-            self._assumed_role = False
+        self._aws_credentials = credentials
+        self._assumed_role = boolify(self._aws_credentials.get('aws_access_key', False))
 
     def _run_packer_cmd(self, cmd):
+        """
+        Triggers the Packer command line
+
+        :param cmd: command arguments
+        :return: The result of the command execution
+        """
         result = ""
         packer_env = os.environ.copy()
         if not os.path.isdir(PACKER_LOGDIR):
             os.makedirs(PACKER_LOGDIR)
         packer_env['TMPDIR'] = PACKER_LOGDIR
         if self._assumed_role:
-            packer_env['AWS_ACCESS_KEY_ID'] = self.packer_config['aws_access_key']
-            packer_env['AWS_SECRET_ACCESS_KEY'] = self.packer_config['aws_secret_key']
-            packer_env['AWS_SESSION_TOKEN'] = self.packer_config['token']
-            packer_env['AWS_SECURITY_TOKEN'] = self.packer_config['token']
+            packer_env['AWS_ACCESS_KEY_ID'] = self._aws_credentials['aws_access_key']
+            packer_env['AWS_SECRET_ACCESS_KEY'] = self._aws_credentials['aws_secret_key']
+            packer_env['AWS_SESSION_TOKEN'] = self._aws_credentials['token']
+            packer_env['AWS_SECURITY_TOKEN'] = self._aws_credentials['token']
         process = Popen(cmd, stdout=PIPE, env=packer_env)
         while True:
             output = process.stdout.readline()
@@ -43,9 +43,9 @@ class Packer:
                     target = out_tab[1]
                     msg_type = out_tab[2]
                     data = out_tab[3]
-                    if (msg_type == "ui" and len(out_tab) > 4):
+                    if msg_type == "ui" and len(out_tab) > 4:
                         log("{0}".format(out_tab[4]), self._log_file)
-                    elif (msg_type == "artifact"):
+                    elif msg_type == "artifact":
                         if len(out_tab) > 4 and out_tab[4] == "id":
                             result = out_tab[5]
                             log("AMI: {0}".format(result), self._log_file)
@@ -55,7 +55,6 @@ class Packer:
         return rc, result
 
     def build_image(self, packer_file_path):
-
         ret_code, result = self._run_packer_cmd(
                                         [
                                             'packer',
@@ -64,8 +63,6 @@ class Packer:
                                             packer_file_path
                                         ]
                                     )
-        if (ret_code == 0):
-            ami = result.split(':')[1]
-        else:
-            ami = "ERROR"
-        return ami
+        # Packer returns a line formatted as {aws-region}:{ami-id}
+        ami_id = result.split(':')[1] if ret_code == 0 else None
+        return ami_id or "ERROR"
