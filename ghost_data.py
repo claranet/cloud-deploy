45890added1c55f52fa2a6422832c492b5ea0beb
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-07-12 16:29:40 +0200

    GHOST-406: Update Data layer with normalization at one place

--- ghost_data.py
+++ ghost_data.py
@@ -14,6 +14,11 @@ def get_db_connection():
     return MongoClient(host=MONGO_HOST, port=MONGO_PORT)[MONGO_DBNAME]
 
 
+def get_deployments_db():
+    db = get_db_connection()
+    return db.deployments
+
+
 def close_db_connection():
     MongoClient().close()
 
@@ -25,7 +30,7 @@ def get_app(app_id):
     db = get_db_connection()
     app = db.apps.find_one({'_id': ObjectId(app_id)})
     close_db_connection()
-    return app
+    return normalize_app(app)
 
 
 def get_job(job_id):
@@ -35,3 +40,57 @@ def get_job(job_id):
     job = db.jobs.find_one({'_id': ObjectId(job_id)})
     close_db_connection()
     return job
+
+
+# Data normalization
+def normalize_app(app, embed_last_deployment=False):
+    """
+    eve post-fetch app event hook to normalize special fields, mostly with models breaking changes
+
+    >>> app_logs = {}
+    >>> normalize_app(app_logs)
+    >>> app_logs.get('log_notifications')
+    []
+
+    >>> app_logs = {'log_notifications': [
+    ...     'no-reply@fr.clara.net',
+    ...     'dummy@fr.clara.net',
+    ... ]}
+    >>> normalize_app(app_logs)
+    >>> [sorted(l.items()) for l in app_logs.get('log_notifications')]
+    [[('email', 'no-reply@fr.clara.net'), ('job_states', ['*'])], [('email', 'dummy@fr.clara.net'), ('job_states', ['*'])]]
+
+    >>> app_logs = {'log_notifications': [
+    ...     {'email': 'no-reply@fr.clara.net', 'job_states': ['done']},
+    ...     'dummy@fr.clara.net',
+    ... ]}
+    >>> normalize_app(app_logs)
+    >>> [sorted(l.items()) for l in app_logs.get('log_notifications')]
+    [[('email', 'no-reply@fr.clara.net'), ('job_states', ['done'])], [('email', 'dummy@fr.clara.net'), ('job_states', ['*'])]]
+    """
+    # Retrieve each module's last deployment
+    for module in app.get('modules', []):
+        query = {
+            '$and': [
+                {'app_id': app['_id']},
+                {'module': module['name']}
+            ]
+        }
+        sort = [('timestamp', -1)]
+        deployment = get_deployments_db().find_one(query, sort=sort)
+        if deployment:
+            module['last_deployment'] = deployment if embed_last_deployment else deployment['_id']
+
+    # Normalize log_notifications
+    log_notifications = []
+    for notif in app.get('log_notifications', []):
+        if isinstance(notif, basestring):
+            log_notifications.append({
+                'email': notif,
+                'job_states': ['*']
+            })
+        else:
+            log_notifications.append(notif)
+    app['log_notifications'] = log_notifications
+
+    return app
