62f7949ec1ea4661307b88c09044c05fa5ae982d
Patrick Decat <patrick.decat@fr.clara.net>
2018-12-13 18:09:45 +0100

    GHOST-699: use more explicit variable names and rename tar archive at end to allow deletion at next execution

--- scripts/backup_ghost.sh
+++ scripts/backup_ghost.sh
@@ -23,8 +23,9 @@ SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 CONFIG="$SCRIPTPATH/../config.yml"
 S3=$(grep bucket_s3 $CONFIG | cut -d' ' -f2)
 S3_REGION=$(grep bucket_region $CONFIG | cut -d' ' -f2)
-BACKUP=/ghost/.backup_ghost
-FILE="ghost-backup-`date +"%F_%H_%M"`.tar.gz"
+BACKUP_DIR=/ghost/.backup_ghost
+BACKUP_FILE="ghost-backup-`date +"%F_%H_%M"`.tar.gz"
+BACKUP_FILE_PREVIOUS="ghost-backup-previous.tar.gz"
 
 if [ $# -eq 1 ]; then
     S3_REGION=$1
@@ -33,12 +34,12 @@ elif [ -z "$S3_REGION" ]; then
 fi
 
 # Cleanup previous execution
-rm -rf $BACKUP
-rm -f $FILE
+rm -rf $BACKUP_DIR
+rm -rf $BACKUP_FILE_PREVIOUS
 
 # Create backup dir
-mkdir $BACKUP
-cd $BACKUP
+mkdir $BACKUP_DIR
+cd $BACKUP_DIR
 
 # Dump mongodb
 mongodump
@@ -61,7 +62,10 @@ ln -snf /etc/supervisor/conf.d/* ./supervisor/
 
 # Create archive
 cd ..
-tar --dereference -czf $FILE $BACKUP
+tar --dereference -czf $BACKUP_FILE $BACKUP_DIR
 
 # Upload archive to S3
-/usr/local/bin/aws s3 cp --only-show-errors $FILE --region $S3_REGION s3://$S3/backup/
+/usr/local/bin/aws s3 cp --only-show-errors $BACKUP_FILE --region $S3_REGION s3://$S3/backup/
+
+# Rename previous backup file to keep it until next backup
+mv -f $BACKUP_FILE $BACKUP_FILE_PREVIOUS
