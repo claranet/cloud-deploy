bcc63244a5f352b5caaecf295bb27c484e9c9a8e
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-01-31 18:15:25 +0100

    GHOST-701: Uses default provisionners options if not provided  - Salt bootstrap option

--- libs/provisioners/__init__.py
+++ libs/provisioners/__init__.py
@@ -1,9 +1,8 @@
 from ghost_log import log
 from ghost_tools import GCallException, get_provisioners_config
-
 from .provisioner import FeaturesProvisioner
-from .provisioner_salt import FeaturesProvisionerSalt
 from .provisioner_ansible import FeaturesProvisionerAnsible
+from .provisioner_salt import FeaturesProvisionerSalt
 
 
 def get_provisioners(config, log_file, unique, job_options, app):
@@ -20,11 +19,14 @@ def get_provisioners(config, log_file, unique, job_options, app):
     """
     ret = []
     provisioners_config = get_provisioners_config(config)
+    # Skip_salt_bootstrap option with default value if not set.
+    job_options = job_options or [config.get('skip_provisioner_bootstrap', True)]
     for key, provisioner_config in provisioners_config.iteritems():
         if key == 'salt':
             ret.append(FeaturesProvisionerSalt(log_file, unique, job_options, provisioner_config, config))
         elif key == 'ansible':
-            ret.append(FeaturesProvisionerAnsible(log_file, unique, app['build_infos']['ssh_username'], provisioner_config, config))
+            ret.append(FeaturesProvisionerAnsible(log_file, unique, app['build_infos']['ssh_username'],
+                                                  provisioner_config, config))
         else:
             log("Invalid provisioner type. Please check your yaml 'config.yml' file", log_file)
             raise GCallException("Invalid features provisioner type")
